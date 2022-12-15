674e8894577166a161cd26c2ef4421bf984194da
Stefan Boskamp <stboskamp@gmail.com>
2022-12-15 20:16:09 +0100

    add summary

--- js/summary.js
+++ js/summary.js
@@ -15,7 +15,19 @@ async function renderSummary() {
             <h4>Everything in a nutshell!</h4>
         </div>
         <div class='welcome'>
-            
+            <h4>Good Morning</h4>
+            <h3>Sofia Müller</h3>
+        </div>
+        <div class="overview">
+            <div class='task_overview'>
+                <button id='taskInBoard'><span id='taskInBoardAmount'>5</span> <p>Task in Board</p></button>
+                <button id='taskInProgress'><span id='taskInProgressAmount'>2</span> <p>Task in Progress</p></button>
+                <button id='awaitingFeedback'><span id='awaitingFeedbackAmount'>2</span> <p>Awaiting Feedback</p></button>
+            </div>
+            <div class='ugency_summary'></div>
+            <div class='to_do'></div>
         </div>
     `;
 }
+
+
