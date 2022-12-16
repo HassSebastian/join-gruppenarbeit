eac72280cca78dc971c7476e10dc2cad1b018e02
Stefan Boskamp <stboskamp@gmail.com>
2022-12-16 19:46:41 +0100

    legal notice layout created

---
+++ js/legal_notice.js
@@ -0,0 +1,44 @@
+async function initLegalNotice() {
+    await includeHTML();
+    selectedMenuButton(1);
+    renderLegalNotice();
+}
+
+
+async function renderLegalNotice() {
+    document.getElementById('content').innerHTML = '';
+    document.getElementById('content').innerHTML += /*html*/ `
+        <div class='headerLegal'>
+            <h2>Legal Notice</h2>
+        </div>
+        <div class='lawTextFrame'>
+            <div class='lawTextSection'>
+                <h3>Subtitle</h3>
+                <p>Lorem ipsum dolor sit amet, consetetur sadipscing elitr, sed
+                    diam nonumy eirmod tempor invidunt ut labore et dolore magna
+                    aliquyam erat, sed diam voluptua. At vero eos et accusam et 
+                    justo duo dolores et ea rebum. Stet clita kasd gubergren, no 
+                    sea takimata sanctus est Lorem ipsum dolor sit amet. Lorem 
+                    ipsum dolor sit amet, consetetur sadipscing elitr, sed diam 
+                    nonumy eirmod tempor invidunt ut labore et dolore magna aliquyam 
+                    erat, sed diam voluptua. At vero eos et accusam et justo duo dolores 
+                    et ea rebum. Stet clita kasd gubergren, no sea takimata sanctus est 
+                    Lorem ipsum dolor sit amet.</p>
+            </div>
+            <div class='lawTextSection'>
+                <h3>Subtitle</h3>
+                <p>Lorem ipsum dolor sit amet, consetetur sadipscing elitr, sed
+                    diam nonumy eirmod tempor invidunt ut labore et dolore magna
+                    aliquyam erat, sed diam voluptua. At vero eos et accusam et 
+                    justo duo dolores et ea rebum. Stet clita kasd gubergren, no 
+                    sea takimata sanctus est Lorem ipsum dolor sit amet. Lorem 
+                    ipsum dolor sit amet, consetetur sadipscing elitr, sed diam 
+                    nonumy eirmod tempor invidunt ut labore et dolore magna aliquyam 
+                    erat, sed diam voluptua. At vero eos et accusam et justo duo dolores 
+                    et ea rebum. Stet clita kasd gubergren, no sea takimata sanctus est 
+                    Lorem ipsum dolor sit amet.</p>
+            </div>
+
+        </div>
+    `;
+}
--- js/summary.js
+++ js/summary.js
@@ -1,4 +1,4 @@
-async function init_summary() {
+async function initSummary() {
     await includeHTML();
     selectedMenuButton(1);
     renderSummary();
