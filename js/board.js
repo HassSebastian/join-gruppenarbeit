dec66316449127a63afa26ddcfae5c4321b9f7ea
HassSebastian <sebastian.hass76@gmail.com>
2022-12-18 15:05:44 +0100

    board progress

--- js/board.js
+++ js/board.js
@@ -76,16 +76,61 @@ async function renderBoard() {
     <div class="canbanBoard">
 
         <div class="canbanContainer">
+
+            <div class="menu todo">
+                <span>To do</span>
+                <button class="menuPlusButton">
+                    <img src="./assets/img/plus_logo_black.png">
+                </button>
+            </div>
+
             <div class="taskBackground">
                 <div class="taskContainer">
                     <div class="taskKategorie design">
                         <span>Design</span>
                     </div>
+
+                    <div class="taskHeadline">
+                        <span class="taskHeadlineContent">Website redesign</span>
+                        <span class="taskContent">Modify the contents of the main website...</span>
+                    </div>
+
+                    <div class="doneBar">
+                        <div></div>
+                        <span>1/2 Done</span>
+                    </div>
+
+                    <div class="contributorsPrio">
+                        <div class="contributorsLogoContainer">
+                            <div class="contributorsLogoBlue">
+                                <span>SM</span>
+                            </div>
+                            <div class="contributorsLogoPink">
+                                <span>MV</span>
+                            </div>
+                            <div class="contributorsLogoGreen">
+                                <span>EF</span>
+                            </div>
+                        </div>
+
+                        <div class="prio">
+                        </div>
+                    </div>
+
                 </div>
             </div>
+
         </div>
 
         <div class="canbanContainer">
+
+            <div class="menu progress">
+                <span>In progress</span>
+                <button class="menuPlusButton">
+                    <img src="./assets/img/plus_logo_black.png">
+                </button>
+            </div>
+
             <div class="taskBackground">
                 <div class="taskContainer">
                     <div class="taskKategorie sales">
@@ -93,9 +138,18 @@ async function renderBoard() {
                     </div>
                 </div>
             </div>
+
         </div>
 
         <div class="canbanContainer">
+
+            <div class="menu feedback">
+                <span>Anwaiting Feedback</span>
+                <button class="menuPlusButton">
+                    <img src="./assets/img/plus_logo_black.png">
+                </button>
+            </div>
+
             <div class="taskBackground">
                 <div class="taskContainer">
                     <div class="taskKategorie backoffice">
@@ -103,9 +157,18 @@ async function renderBoard() {
                     </div>
                 </div>
             </div>
+
         </div>
 
         <div class="canbanContainer">
+
+            <div class="menu done">
+                <span>Done</span>
+                <button class="menuPlusButton">
+                    <img src="./assets/img/plus_logo_black.png">
+                </button>
+            </div>
+
             <div class="taskBackground">
                 <div class="taskContainer">
                     <div class="taskKategorie marketing">
@@ -113,6 +176,7 @@ async function renderBoard() {
                     </div>
                 </div>
             </div>
+
         </div>
 
     </div>
