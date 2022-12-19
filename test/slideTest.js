276aba33c7e5d14c49f445705cd9565f8d840484
HassSebastian <sebastian.hass76@gmail.com>
2022-12-19 12:26:04 +0100

    slideTest-Animation finish

--- test/slideTest.js
+++ test/slideTest.js
@@ -1,7 +1,10 @@
 
 function initPage1(){
-
-
-    
+    document.getElementById('page1_logo').classList.add('page1joinLogoContainerMove');
+    setTimeout(startLogIn, 700);
+}
+function startLogIn(){
+    document.getElementById('page1').classList.add('d-none');
+    document.getElementById('logIn').classList.remove('d-none');
 }
 
