0bbe9bf3bf03e2af3839497ca7c243803e45b40f
Dorian Jacek Dell'Anna <dellanna.youtube@web.de>
2022-12-21 16:12:08 +0100

    New contact Button bearbeitet

--- js/contacts.js
+++ js/contacts.js
@@ -7,33 +7,6 @@ async function initContacts() {
 async function renderContacts() {
     document.getElementById('content').innerHTML = '';
     document.getElementById('content').innerHTML += /*html*/ `
-<div class="Frame_97">
-        <div class="Contact_list">
-            <div class="letters">
-               <span>A</span>
-            </div>
-            <div class="Vector_10"></div>
-            <div class="contacts">
-                <div class="contact">
-                    <div class="ellipse">
-                        <span>AM</span>
-                    </div>
-                    <div class="name_and_email">
-                        <div class="name">
-                            <span>Anton Mayer</span>
-                        </div>
-                        <div class="email">
-                            <span>antom@gmail.com</span>
-                        </div>
-                    </div>
-                </div>
-            </div>
-        </div>
-    </div>
-    <div class="better_with_a_team">
-        <h1>Contacts</h1>
-        <div class="vector_5"></div>
-        <span>Better with a team</span>
-    </div>
+
     `;
 }
