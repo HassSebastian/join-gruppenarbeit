c85cf2dbb75b81bca42ffdc9008376f66a3311dc
HassSebastian <sebastian.hass76@gmail.com>
2022-12-23 14:54:28 +0100

    signUp in progress

--- js/signUp.js
+++ js/signUp.js
@@ -6,9 +6,8 @@ function backToLogIn() {
 }
 
 function userSignIn() {
-    let name = document.getElementById('inputNameSignUp');
-    let email = document.getElementById('inputEmailSignUp');
-    let password = document.getElementById('inputPasswordSignUp');
+
+    inputValueTest(name, email, password);
 
     // edited by Bossi
     // first you have to load the file from localStorage before you edited this.
@@ -16,10 +15,40 @@ function userSignIn() {
     allUsers = JSON.parse(allUsersString);
     // edited by Bossi end
 
-    allUsers.push({'name': name.value, 'email': email.value, 'password': password.value});
+    allUsers.push({ 'name': name.value, 'email': email.value, 'password': password.value });
 
     let allUsersAtString = JSON.stringify(allUsers);
     localStorage.setItem('allUsers', allUsersAtString);
-    
+
     window.location.href = './login.html?msg=Du hast dich erfolgreich registriert';
+}
+
+function inputValueTest() {
+    document.querySelector('#requiredName', '#requiredEmail', '#requiredPassword').classList.remove('requiredOn');
+
+    let name = document.getElementById('inputNameSignUp');
+    let email = document.getElementById('inputEmailSignUp');
+    let password = document.getElementById('inputPasswordSignUp');
+
+    if (!name.value.length || !email.value.length || !password.value.length) {
+        if (name.value.length == 0) {
+            document.getElementById('requiredName').classList.add('requiredOn');
+        } else {
+            document.getElementById('requiredName').classList.remove('requiredOn');
+        };
+        if (email.value.length <= 8) {
+            document.getElementById('requiredEmail').classList.add('requiredOn');
+        } else {
+            document.getElementById('requiredEmail').classList.remove('requiredOn');
+        };
+        if (password.value.length == 0) {
+            document.getElementById('requiredPassword').classList.add('requiredOn');
+        } else {
+            document.getElementById('requiredPassword').classList.remove('requiredOn');
+        }
+        ;
+    }else{
+        console.log('weiter');
+    };
+    console.log('fehler');
 }
