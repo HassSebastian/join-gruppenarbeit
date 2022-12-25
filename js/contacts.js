80ca06d5e2319651fb8c681ce3d3ff6e978611be
Dorian Jacek Dell'Anna <dellanna.youtube@web.de>
2022-12-25 14:17:22 +0100

    Funktion zum bearbeiten der Kontakte hinzugefügt

--- js/contacts.js
+++ js/contacts.js
@@ -38,6 +38,12 @@ contacts.sort(function (a, b) {
     return compareStrings(a.name, b.name);
 })
 
+function sortContacts() {
+    contacts.sort(function (a, b) {
+        return compareStrings(a.name, b.name);
+    })
+}
+
 async function renderContacts() {
     document.getElementById('contacts').innerHTML = '';
 
@@ -80,6 +86,44 @@ function closeNewContact() {
     renderContacts();
 }
 
+function openEditContact(i) {
+    document.getElementById('edit_contact').classList.remove('d-none')
+    document.getElementById('edit_contact').innerHTML /*html*/ = '';
+    document.getElementById('edit_contact').innerHTML = /*html*/ `
+    <div class="add_contact_left">
+    <img src="assets/img/join_logo.png" alt="">
+    <h1>Add contact</h1>
+    <span>Tasks are better with a team!</span>
+    <div class="add_contact_vector_5 "></div>
+</div>
+<div class="add_contact_right">
+    <img src="assets/img/empty_profile_picture.png" alt="">
+    <div class="add_contact_inputs">
+        <input type="text" placeholder="Name" id="editContactName">
+        <input type="text" placeholder="Surname" id="editContactSurname">
+        <input type="text" placeholder="Email" id="editContactEmail">
+        <input type="text" placeholder="Phone" id="editContactPhone">
+        <div class="add_contact_right_buttons">
+            <div class="contact_cancel" onclick="closeEditContact()">
+                <span>Cancel</span>
+                <img src="assets/img/new_cat_cancel.png" alt="">
+            </div>
+            <div class="contact_create" onclick="editContact(${i})">
+                <span>Update contact</span>
+                <img src="assets/img/akar-icons_check_white.png" alt="">
+            </div>
+
+        </div>
+    </div>
+    `;
+    renderContacts();
+}
+
+function closeEditContact() {
+    document.getElementById('edit_contact').classList.add('d-none');
+    renderContacts();
+}
+
 function showContact(i) {
     const surname = contacts[i].surname;
     const name = contacts[i].name;
@@ -102,7 +146,7 @@ function showContact(i) {
             <div class="contact_information">
                 <div class="contact_information_edit">
                     <h2>Contact Information</h2>
-                    <div class="edit_contact">
+                    <div class="edit_contact" onclick="openEditContact(${i})">
                         <img src="assets/img/edit_button_black.png" alt="">
                         <span>Edit Contact</span>
                     </div>
@@ -120,11 +164,25 @@ function showContact(i) {
 function addContact() {
     var contact = {};
 
-    contact.name = document.getElementById("contactName").value;
-    contact.surname = document.getElementById("contactSurname").value;
-    contact.email = document.getElementById("contactEmail").value;
-    contact.phone = document.getElementById("contactPhone").value;
+    contact.name = document.getElementById('contactName').value;
+    contact.surname = document.getElementById('contactSurname').value;
+    contact.email = document.getElementById('contactEmail').value;
+    contact.phone = document.getElementById('contactPhone').value;
 
-    contacts.push(contact);  
+    contacts.push(contact);
+    sortContacts();
     closeNewContact();
+}
+
+function editContact(i) {
+    var contact = {};
+
+    contact.name = document.getElementById('editContactName').value;
+    contact.surname = document.getElementById('editContactSurname').value;
+    contact.email = document.getElementById('editContactEmail').value;
+    contact.phone = document.getElementById('editContactPhone').value;
+
+    contacts.splice(i, 1, contact);
+    sortContacts();
+    closeEditContact();
 }
