d9b1524fb806a0a0a5167707a23a3372cd876fff
Stefan Boskamp <stboskamp@gmail.com>
2022-12-25 19:53:33 +0100

    board popup addTask add

--- js/board.js
+++ js/board.js
@@ -54,7 +54,7 @@ function boardHtml(){
                         <img src="./assets/img/search_logo.png">
                     </div>
                 </div>
-                <button class="addTaskButton">
+                <button class="addTaskButton" onclick='showAddTaskPopupWindow()'>
                     <span>Add task</span>
                     <div class="plusOutContainer">
                         <img src="./assets/img/plus_logo_white.png">
@@ -173,6 +173,8 @@ function boardHtml(){
 
 // Testarea PopupCard ***********************************************************************************************
 
+// basic function for the popup window
+
 /**
  * this function remove the d-none class from the popup window. The result is that the Popup Window is shown.
  */
@@ -197,14 +199,16 @@ function stopClose(event){
 }
 
 
+// basic function popup end
+
+
+// render function for the detail view of the task card.
 function renderPopupTaskCard(taskIndex){
-    console.log('rendering started for ', taskIndex)
     let cardTitle = joinTaskArray[taskIndex]['title'];
     let cardDescription = joinTaskArray[taskIndex]['descripten'];
     let cardCategory = joinTaskArray[taskIndex]['category'];
     let cardDueDate = joinTaskArray[taskIndex]['dueDate'];
     let taskPrio = joinTaskArray[taskIndex]['prio'];
-    console.log(cardTitle, cardDescription, cardCategory, cardDueDate, taskPrio);
     document.getElementById('boardPopup').innerHTML = '';
     document.getElementById('boardPopup').innerHTML = /*html*/`
         <div class="boardTaskCardPopup" onclick='stopClose(event)'>
@@ -264,11 +268,17 @@ function renderPopupTaskCard(taskIndex){
 }
 
 
+/**
+ * 
+ * @param {number} taskIndex -this value is equal to the index number of the main array "joinTaskArray", where
+ * the task card information is stored.
+ */
 function setTaskCardPopupCatColor(taskIndex){
     let cardCatColorIndex = joinTaskArray[taskIndex]['catColor'];
     let cardCatColor = categoryBackgroundColors[cardCatColorIndex];
     document.getElementById('taskCardPopupCategory').style = `background-color: ${cardCatColor};`;
 }
+
 
 let prioColorAndUrlArray = [
     {
@@ -279,6 +289,11 @@ let prioColorAndUrlArray = [
 ]
 
 
+/**
+ * This function sets the background color of the prio button and transfers the url of the image associated with the prio button.
+ * @param {*} taskIndex --this value is equal to the index number of the main array "joinTaskArray", where
+ * the task card information is stored.
+ */
 function setTaskCardPopupPrioBackground(taskIndex){
     let cardPrio = joinTaskArray[taskIndex]['prio'];
     cardPrio = cardPrio.toLowerCase();
@@ -287,6 +302,151 @@ function setTaskCardPopupPrioBackground(taskIndex){
     document.getElementById('prioContainer').style = `background-color: ${cardPrioBackground};`;
     document.getElementById('cardPrioImg').src = cardPrioImgSrc;
 }
+// render function for the detail view of the task card end.
+
+
+// render function for the creation of a new task card.
+async function showAddTaskPopupWindow(){
+    enablePopupWindow();
+    await renderAddTaskPopup();
+    loadExitingCategories();
+    renderCategoryList();
+    newCatInputActive = false;
+    renderSubtasks();
+}
+
+
+async function renderAddTaskPopup(){
+    document.getElementById('boardPopup').innerHTML = '';
+    document.getElementById('boardPopup').innerHTML = /*html*/`
+        <div id='boardAddTaskPopup' onclick='stopClose(event)'>
+            <div class='boardAddTaskHeadlineDiv'>
+                <h2 class='addTHeadline'>Add Task</h2>
+            </div>
+            <div class='boardAddTaskBtnOuterContainer'>
+                <div class='boardAddTaskBtnInnerContainer'>
+                    <button class='addTaskClear' onmouseover='addTaskClearOn()' onmouseout='addTaskClearOff()' onclick='clearFormularData()'>
+                        <span>Clear</span> 
+                        <img id="addTaskClear" src="./assets/img/clearb.png">
+                    </button>
+                    <button class='addTaskCreate' onclick='checkInputs()'>
+                        <span>Create Task</span>
+                        <img src="./assets/img/createb.png">  
+                    </button>
+                </div>
+            </div>
+            <div class='boardAddTaskAddTitleContainer'>
+                <div class='addTaskAddTitleBox'>
+                    <h3>Title</h3>
+                    <form onsubmit='goToDescripten(); return false' >
+                        <input  required type="text" placeholder='Enter a title' id='addTaskTitle' autocomplete='off' minlength='3'>
+                    </form>
+                    <span class='requiredText' id='titleReq'>This field is required</span>
+                </div>
+
+                <div class='addTaskAddDescriptenBox'>
+                    <h3>Descripten</h3>
+                    <!-- <form onblur='goToCategorySelection(); return false' id='formDesc' action='action.asp'> -->
+                    <textarea form='formDesc' type="text" placeholder='Enter Descripten' id='addTaskDescripten'  required minlength='5'></textarea>
+                    <!-- </form> -->
+                    <span class='requiredText' id='descReq'>This field is required</span>
+                </div>
+
+                <div class='addTaskAddCategoryBox'>
+                    <h3>Category</h3>
+                    <button onclick=enableDisableCatList() id='selectedCat'>
+                        <input disabled id='selectedCatInput' placeholder='Select task category' autocomplete='off'>
+                        <span id='sColor'></span>
+                        <div class='newCategoryImgDiv d-none' id='addTaskNewCatBtn'>
+                            <img src="../assets/img/new_cat_cancel.png">
+                            <img src="../assets/img/bnt_divider.png" class='btnDivider'>
+                            <img src="../assets/img/akar-icons_check.png">
+                        </div>
+                        <img src="../assets/img/Vector 2.png" class='dropdownImg' id='dropdownImg'>
+                    </button>
+                    <span class='listD-none requiredText' id='catReq'>This field is required</span>
+                    <ul class="addTaskCatList listD-none" id='CatListDropdown'>
+                        
+                    </ul>
+                    <div class='addTaskAddCategoryColor listD-none' id='colorSelection'>
+                        <div class='color0' id='color0Div' onclick='addColorToCat(0)'></div>
+                        <div class='color1' id='color1Div' onclick='addColorToCat(1)'></div>
+                        <div class='color2' id='color2Div' onclick='addColorToCat(2)'></div>
+                        <div class='color3' id='color3Div' onclick='addColorToCat(3)'></div>
+                        <div class='color4' id='color4Div' onclick='addColorToCat(4)'></div>
+                        <div class='color5' id='color5Div' onclick='addColorToCat(5)'></div>
+                    </div>
+                </div>
+
+                <div class='addTaskAssignedBox' id='addTaskAssignedBox'>
+                    <h3>Assigned to</h3>
+                    <button onclick=enableDisableAssignList()><span id='selectedAssign'>Select contacts to Assign</span><img src="../assets/img/Vector 2.png" class='dropdownImg'></button>
+                    <span id='assignReq'>This field is required</span>
+                    <ul class="addTaskAssignList listD-none" id="dropdown2">
+                        <li>New Assign</li>
+                        <li>Assign 2</li>
+                        <li>Assign 3</li>
+                    </ul>
+                </div>
+            </div>
+
+            <div class='boardAddTaskDivider'>
+
+            </div>
+
+            <div class='boardAddTaskRightContainer'>
+                <div class='addTaskDate'>
+                    <h3>Due date</h3>
+                    <input required type="date" id='dueDate'>
+                    <span class='requiredText' id='dateReq'>This field is required</span>
+                </div>
+                <div class='addTaskPrio'>
+                    <h3>Prio</h3>
+                    <div class='addTaskPrioIcons'>
+                        <div class='addTaskUrgent' id='addTaskUrgent' onclick='addPrio(0)'>
+                            <span id='addTaskUrgentSpan'>Urgent</span>
+                            <img id='addTaskUrgentImg' src="../assets/img/urgent_arrows.png">
+                        </div>
+                        <div class='addTaskMedium' id='addTaskMedium' onclick='addPrio(1)'>
+                            <span id='addTaskMediumSpan'>Medium</span>
+                            <img id='addTaskMediumImg' src="../assets/img/prio_medium.png">
+                        </div>
+                        <div class='addTaskLow' id='addTaskLow' onclick='addPrio(2)'>
+                            <span id='addTaskLowSpan'>Low</span>
+                            <img id='addTaskLowImg' src="../assets/img/prio_low.png">
+                        </div>
+                    </div>
+                </div>
+                <div class='subtask'>
+                    <h3>Subtask</h3>
+                    <div class='inputDiv'>
+                        <form onsubmit='addSubtask(); return false' >
+                            <input type="text" placeholder='Add new subtask' id='subTask' autocomplete='off' onfocus='subTaskInputentered()' onblur='subTaskInputLeave()' minlength='3'>
+                            <img src="../assets/img/add_cross.png" class='subtaskCross' id='subtaskCross' onclick='enterSubTaskInput()'>
+                        </form>
+                        <div class='subTaskImgDiv d-none' id='subTaskImgDiv' >
+                            <img src="../assets/img/new_cat_cancel.png" onclick='resetSubtaskInput()'>
+                            <img src="../assets/img/bnt_divider.png" class='btnDivider'>
+                            <img src="../assets/img/akar-icons_check.png" onclick='addSubtask()'>
+                        </div>
+                        
+                    </div>
+                    
+                    <div class='addTaskCheckbox' id='subtaskCheckboxes'>
+        
+                    </div>
+                </div>
+            </div>
+        </div>
+        <!-- </form>  -->
+        <div class="taskAddedToBoard" id='test'>
+            <div class="taskAddedToBoardContainer">
+                <span>Task added to board</span>
+                <img src="./assets/img/img_board_w.png">
+            </div>
+        </div>`;
+}
+
 
 // Testarea PopupCard end***********************************************************************************************
 
