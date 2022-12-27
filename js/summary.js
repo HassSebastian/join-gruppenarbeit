ad8c5a2d7c8c36e9ef39819c6fa007e0f80f0b1d
HassSebastian <sebastian.hass76@gmail.com>
2022-12-27 14:41:31 +0100

    Date and greet addid

--- js/summary.js
+++ js/summary.js
@@ -3,6 +3,8 @@ async function initSummary() {
     await renderSummary();
     selectedMenuBtnId = 0;
     selectedMenuButton(1);
+    showDate();
+    showTime();
 }
 
 
@@ -16,7 +18,7 @@ async function renderSummary() {
             <h4>Everything in a nutshell!</h4>
         </div>
         <div class='welcome'>
-            <h4>Good Morning</h4>
+            <h4 id='greetUser'></h4>
             <h3>Sofia Müller</h3>
         </div>
         <div class="overview">
@@ -63,7 +65,7 @@ async function renderSummary() {
 
 
 // Hover ToDo and Done Button
-function toDoHoverOn(){
+function toDoHoverOn() {
     document.getElementById('toDoImg').src = "./assets/img/to_do_pen_black.png";
     document.getElementById('toDo').classList.add('toDoHover');
     document.getElementById('toDoAmountTasks').classList.add('toDoHoverSpanP');
@@ -71,7 +73,7 @@ function toDoHoverOn(){
 }
 
 
-function toDoHoverOff(){
+function toDoHoverOff() {
     document.getElementById('toDoImg').src = "./assets/img/to_do_pen.png";
     document.getElementById('toDo').classList.remove('toDoHover');
     document.getElementById('toDoAmountTasks').classList.remove('toDoHoverSpanP');
@@ -79,7 +81,7 @@ function toDoHoverOff(){
 }
 
 
-function toDoDoneHoverOn(){
+function toDoDoneHoverOn() {
     document.getElementById('toDoDoneImg').src = "./assets/img/done_black.png";
     document.getElementById('toDoDone').classList.add('toDoHover');
     document.getElementById('toDoDoneAmountTasks').classList.add('toDoHoverSpanP');
@@ -87,9 +89,43 @@ function toDoDoneHoverOn(){
 }
 
 
-function toDoDoneHoverOff(){
+function toDoDoneHoverOff() {
     document.getElementById('toDoDoneImg').src = "./assets/img/done.png";
     document.getElementById('toDoDone').classList.remove('toDoHover');
     document.getElementById('toDoDoneAmountTasks').classList.remove('toDoHoverSpanP');
     document.getElementById('toDoDoneAmountP').classList.remove('toDoHoverSpanP');
 }
+
+// show date in Summary 
+function showDate() {
+    let currentDate = new Date();
+    let day = currentDate.getDate();
+    let month = currentDate.getMonth() + 1; // Januar ist der 0. Monat
+    let year = currentDate.getFullYear();
+    if (day < 10) {
+        day = "0" + day;
+    }
+    if (month < 10) {
+        month = "0" + month;
+    }
+    let dateString = currentDate.toLocaleDateString('en-US', { month: 'long', day: 'numeric', year: 'numeric' });
+    document.getElementById("deadlineDate").innerHTML = dateString;
+}
+
+// greet User
+function showTime() {
+    let currentTime = new Date();
+    let hours = currentTime.getHours();
+    let timeString = hours;
+    if(timeString >= 6){
+        document.getElementById('greetUser').innerHTML = `Good Morning,`;
+    }
+    if(timeString >= 12){
+        document.getElementById('greetUser').innerHTML = `Good Day,`;
+    }
+    if(timeString >= 18){
+        document.getElementById('greetUser').innerHTML = `Good Evening,`;
+    }
+  }
+
+
