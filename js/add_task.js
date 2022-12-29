fb42d29e1c342ddf3bb426659423b3ab447aa3b0
GreenfieldC <101995422+GreenfieldC@users.noreply.github.com>
2022-12-29 23:59:18 +0100

    addAssign fast fertig

--- js/add_task.js
+++ js/add_task.js
@@ -49,6 +49,30 @@ let coworkersToAssignTo = [
 		email: 'sindSaödf@hurz.de',
 		check: false,
 	},
+	{
+		firstName: 'Bichtig',
+		lastName: 'Tuer',
+		email: 'sisdfsadfz.de',
+		check: false,
+	},
+	{
+		firstName: 'Smelly',
+		lastName: 'Arter',
+		email: 'hurz@de',
+		check: false,
+	},
+	{
+		firstName: 'Claudia',
+		lastName: 'Schiffer',
+		email: 'warmaljung@web.de',
+		check: false,
+	},
+	{
+		firstName: 'Wans',
+		lastName: 'Hurst',
+		email: 'wdg@web.de',
+		check: false,
+	},
 ];
 
 let taskForce = []; // team that will be working on the current task
@@ -72,96 +96,96 @@ async function initAddTask() {
 async function renderAddTask() {
 	document.getElementById('content').innerHTML = '';
 	document.getElementById('content').innerHTML += /*html*/ `
-    <div class="addTaskHeadlineDiv">
-	    <h2 class="addTHeadline">Add Task</h2>
-    </div>
-    <div class="addTaskBtnOuterContainer">
-        <div class="addTaskBtnInnerContainer">
-            <button
-                class="addTaskClear"
-                onmouseover="addTaskClearOn()"
-                onmouseout="addTaskClearOff()"
-                onclick="clearFormularData()"
-            >
-                <span>Clear</span>
-                <img id="addTaskClear" src="./assets/img/clearb.png" />
-            </button>
-            <button class="addTaskCreate" onclick="checkInputs()">
-                <span>Create Task</span>
-                <img src="./assets/img/createb.png" />
-            </button>
-        </div>
-    </div>
-    <!-- <form onsubmit="goToPrio();return false"> -->
-    <div class="addTaskAddTitleContainer">
-        <div class="addTaskAddTitleBox">
-            <h3>Title</h3>
-            <form onsubmit="goToDescripten(); return false">
-                <input
-                    required
-                    type="text"
-                    placeholder="Enter a title"
-                    id="addTaskTitle"
-                    autocomplete="off"
-                    minlength="3"
-                />
-            </form>
-            <span class="requiredText" id="titleReq">This field is required</span>
-        </div>
+	<div class="addTaskHeadlineDiv">
+		<h2 class="addTHeadline">Add Task</h2>
+	</div>
+	<div class="addTaskBtnOuterContainer">
+		<div class="addTaskBtnInnerContainer">
+			<button
+				class="addTaskClear"
+				onmouseover="addTaskClearOn()"
+				onmouseout="addTaskClearOff()"
+				onclick="clearFormularData()"
+			>
+				<span>Clear</span>
+				<img id="addTaskClear" src="./assets/img/clearb.png" />
+			</button>
+			<button class="addTaskCreate" onclick="checkInputs()">
+				<span>Create Task</span>
+				<img src="./assets/img/createb.png" />
+			</button>
+		</div>
+	</div>
+	<!-- <form onsubmit="goToPrio();return false"> -->
+	<div class="addTaskAddTitleContainer">
+		<div class="addTaskAddTitleBox">
+			<h3>Title</h3>
+			<form onsubmit="goToDescripten(); return false">
+				<input
+					required
+					type="text"
+					placeholder="Enter a title"
+					id="addTaskTitle"
+					autocomplete="off"
+					minlength="3"
+				/>
+			</form>
+			<span class="requiredText" id="titleReq">This field is required</span>
+		</div>
 
-        <div class="addTaskAddDescriptenBox">
-            <h3>Descripten</h3>
-            <!-- <form onblur='goToCategorySelection(); return false' id='formDesc' action='action.asp'> -->
-            <textarea
-                form="formDesc"
-                type="text"
-                placeholder="Enter Descripten"
-                id="addTaskDescripten"
-                required
-                minlength="5"
-            ></textarea>
-            <!-- </form> -->
-            <span class="requiredText" id="descReq">This field is required</span>
-        </div>
+		<div class="addTaskAddDescriptenBox">
+			<h3>Descripten</h3>
+			<!-- <form onblur='goToCategorySelection(); return false' id='formDesc' action='action.asp'> -->
+			<textarea
+				form="formDesc"
+				type="text"
+				placeholder="Enter Descripten"
+				id="addTaskDescripten"
+				required
+				minlength="5"
+			></textarea>
+			<!-- </form> -->
+			<span class="requiredText" id="descReq">This field is required</span>
+		</div>
 
-        <div class="addTaskAddCategoryBox">
-            <h3>Category</h3>
-            <button onclick="enableDisableCatList()" id="selectedCat">
-                <input
-                    disabled
-                    id="selectedCatInput"
-                    placeholder="Select task category"
-                    autocomplete="off"
-                />
-                <span id="sColor"></span>
-                <div class="newCategoryImgDiv d-none" id="addTaskNewCatBtn">
-                    <img src="assets/img/new_cat_cancel.png" />
-                    <img src="assets/img/bnt_divider.png" class="btnDivider" />
-                    <img src="assets/img/akar-icons_check.png" />
-                </div>
-                <img
-                    src="assets/img/Vector 2.png"
-                    class="dropdownImg"
-                    id="dropdownImg"
-                />
-            </button>
-            <span class="listD-none requiredText" id="catReq"
-                >This field is required</span
-            >
-            <ul class="addTaskCatList listD-none" id="CatListDropdown"></ul>
-            <div class="addTaskAddCategoryColor listD-none" id="colorSelection">
-                <div class="color0" id="color0Div" onclick="addColorToCat(0)"></div>
-                <div class="color1" id="color1Div" onclick="addColorToCat(1)"></div>
-                <div class="color2" id="color2Div" onclick="addColorToCat(2)"></div>
-                <div class="color3" id="color3Div" onclick="addColorToCat(3)"></div>
-                <div class="color4" id="color4Div" onclick="addColorToCat(4)"></div>
-                <div class="color5" id="color5Div" onclick="addColorToCat(5)"></div>
-            </div>
-        </div>
+		<div class="addTaskAddCategoryBox">
+			<h3>Category</h3>
+			<button onclick="enableDisableCatList()" id="selectedCat">
+				<input
+					disabled
+					id="selectedCatInput"
+					placeholder="Select task category"
+					autocomplete="off"
+				/>
+				<span id="sColor"></span>
+				<div class="newCategoryImgDiv d-none" id="addTaskNewCatBtn">
+					<img src="assets/img/new_cat_cancel.png" />
+					<img src="assets/img/bnt_divider.png" class="btnDivider" />
+					<img src="assets/img/akar-icons_check.png" />
+				</div>
+				<img
+					src="assets/img/Vector 2.png"
+					class="dropdownImg"
+					id="dropdownImg"
+				/>
+			</button>
+			<span class="listD-none requiredText" id="catReq"
+				>This field is required</span
+			>
+			<ul class="addTaskCatList listD-none" id="CatListDropdown"></ul>
+			<div class="addTaskAddCategoryColor listD-none" id="colorSelection">
+				<div class="color0" id="color0Div" onclick="addColorToCat(0)"></div>
+				<div class="color1" id="color1Div" onclick="addColorToCat(1)"></div>
+				<div class="color2" id="color2Div" onclick="addColorToCat(2)"></div>
+				<div class="color3" id="color3Div" onclick="addColorToCat(3)"></div>
+				<div class="color4" id="color4Div" onclick="addColorToCat(4)"></div>
+				<div class="color5" id="color5Div" onclick="addColorToCat(5)"></div>
+			</div>
+		</div>
 
-        <div class="addTaskAssignedBox" id="addTaskAssignedBox">
-            <h3>Assigned to</h3>
-            <button id="addTaskAssignedButton" onclick="enableDisableAssignList()">
+		<div class="addTaskAssignedBox" id="addTaskAssignedBox">
+			<h3>Assigned to</h3>
+			<button id="addTaskAssignedButton" onclick="enableDisableAssignList()">
 				<input
 						disabled
 						onclick="doNotCloseOnClick(event)"
@@ -176,15 +200,25 @@ async function renderAddTask() {
 				id="assignToCancelConfirmImgContainer"
 				class="assignToCancelConfirmImgContainer d-none"
 				>
-					<img onclick="assignBoxBackToDefaultMode(), enableAssignList()" class="assignToCancelIcon" src="assets/img/cancel-black.png" alt="cancel" />
-					<img class="assignToDeviderIcon"src="assets/img/bnt_divider.png" />
-					<img class="assignToCheckIcon" src="assets/img/akar-icons_check.png" alt="confirm" />
+					<img
+					onclick="assignBoxBackToDefaultMode(), enableAssignList()"
+					class="assignToCancelIcon"
+					src="assets/img/cancel-black.png"
+					alt="cancel"
+					/>
+					<img class="assignToDeviderIcon" src="assets/img/bnt_divider.png" />
+					<img
+					onclick="frontEndDeveloper()"
+					class="assignToCheckIcon"
+					src="assets/img/akar-icons_check.png"
+					alt="confirm"
+					/>
 				</div>
 				<img id="assignDropDownImg" src="assets/img/Vector 2.png" class="dropdownImg" />
-            </button>
-            <span id="assignReq">This field is required</span>
+			</button>
+			<span id="assignReq">This field is required</span>
 			<div id="badgesTaskForce" class="badgesTaskForce"></div>
-            <ul class="addTaskAssignList listD-none" id="dropdown2">
+			<ul class="addTaskAssignList listD-none" id="dropdown2">
 
 				<li onclick="assigendContactEmail()" class="inviteNewContacts">
 					Invite new contacts<img
@@ -193,68 +227,91 @@ async function renderAddTask() {
 						alt=""
 					/>
 				</li>
+				<li>
+		You
+		<div  class="assignCheckboxContainer">
+			<img class="checkBox" src="assets/img/check_box.png" alt="checkbox" />
+			<img class="checkMark" src="assets/img/check_mark.png" />
+		</div>
+	</li>
 			</ul>
-        </div>
-    </div>
-    <div class="addTaskDivider"></div>
+		</div>
+	</div>
+	<div class="addTaskDivider"></div>
 
-        </div>
+		</div>
 
-        <div class='addTaskRightContainer'>
-            <div class='addTaskDate'>
-                <h3>Due date</h3>
-                <input required type="date" id='dueDate'>
-                <span class='requiredText' id='dateReq'>This field is required</span>
-            </div>
-            <div class='addTaskPrio'>
-                <h3>Prio</h3>
-                <div class='addTaskPrioIcons'>
-                    <div class='addTaskUrgent' id='addTaskUrgent' onclick='addPrio(0)'>
-                        <span id='addTaskUrgentSpan'>Urgent</span>
-                        <img id='addTaskUrgentImg' src="../assets/img/urgent_arrows.png">
-                    </div>
-                    <div class='addTaskMedium' id='addTaskMedium' onclick='addPrio(1)'>
-                        <span id='addTaskMediumSpan'>Medium</span>
-                        <img id='addTaskMediumImg' src="../assets/img/prio_medium.png">
-                    </div>
-                    <div class='addTaskLow' id='addTaskLow' onclick='addPrio(2)'>
-                        <span id='addTaskLowSpan'>Low</span>
-                        <img id='addTaskLowImg' src="../assets/img/prio_low.png">
-                    </div>
-                </div>
-            </div>
-            <div class='subtask'>
-                <h3>Subtask</h3>
-                <div class='inputDiv'>
-                    <form onsubmit='addSubtask(); return false' >
-                        <input type="text" placeholder='Add new subtask' id='subTask' autocomplete='off' onfocus='subTaskInputentered()' onblur='subTaskInputLeave()' minlength='3'>
-                        <img src="../assets/img/add_cross.png" class='subtaskCross' id='subtaskCross' onclick='enterSubTaskInput()'>
-                    </form>
-                    <div class='subTaskImgDiv d-none' id='subTaskImgDiv' >
-                        <img src="../assets/img/new_cat_cancel.png" onclick='resetSubtaskInput()'>
-                        <img src="../assets/img/bnt_divider.png" class='btnDivider'>
-                        <img src="../assets/img/akar-icons_check.png" onclick='addSubtask()'>
-                    </div>
-                    
-                </div>
-                
-                <div class='addTaskCheckbox' id='subtaskCheckboxes'>
-                    <!-- <div>
-                        <input type="checkbox">
-                        <span>Subtask 1</span>
-                    </div> -->
-                </div>
-                
-                
-            </div>
-        </div>
-    <!-- </form>  -->
-    <div class="taskAddedToBoard" id="taskCreatedIndication">
-        <div class="taskAddedToBoardContainer">
-            <span>Task added to board</span>
-            <img src="./assets/img/img_board_w.png" />
-        </div>
-    </div>
+		<div class='addTaskRightContainer'>
+			<div class='addTaskDate'>
+				<h3>Due date</h3>
+				<input required type="date" id='dueDate'>
+				<span class='requiredText' id='dateReq'>This field is required</span>
+			</div>
+			<div class='addTaskPrio'>
+				<h3>Prio</h3>
+				<div class='addTaskPrioIcons'>
+					<div class='addTaskUrgent' id='addTaskUrgent' onclick='addPrio(0)'>
+						<span id='addTaskUrgentSpan'>Urgent</span>
+						<img id='addTaskUrgentImg' src="../assets/img/urgent_arrows.png">
+					</div>
+					<div class='addTaskMedium' id='addTaskMedium' onclick='addPrio(1)'>
+						<span id='addTaskMediumSpan'>Medium</span>
+						<img id='addTaskMediumImg' src="../assets/img/prio_medium.png">
+					</div>
+					<div class='addTaskLow' id='addTaskLow' onclick='addPrio(2)'>
+						<span id='addTaskLowSpan'>Low</span>
+						<img id='addTaskLowImg' src="../assets/img/prio_low.png">
+					</div>
+				</div>
+			</div>
+			<div class='subtask'>
+				<h3>Subtask</h3>
+				<div class='inputDiv'>
+					<form onsubmit='addSubtask(); return false' >
+					<input
+					type="text"
+					placeholder="Add new subtask"
+					id="subTask"
+					autocomplete="off"
+					onfocus="subTaskInputentered()"
+					onblur="subTaskInputLeave()"
+					minlength="3"
+				/>
+				<img
+					src="../assets/img/add_cross.png"
+					class="subtaskCross"
+					id="subtaskCross"
+					onclick="enterSubTaskInput()"
+				/>
+					</form>
+					<div class='subTaskImgDiv d-none' id='subTaskImgDiv' >
+						<img src="../assets/img/new_cat_cancel.png" onclick='resetSubtaskInput()'>
+						<img src="../assets/img/bnt_divider.png" class='btnDivider'>
+						<img src="../assets/img/akar-icons_check.png" onclick='addSubtask()'>
+					</div>
+					
+				</div>
+				
+				<div class='addTaskCheckbox' id='subtaskCheckboxes'>
+					<!-- <div>
+						<input type="checkbox">
+						<span>Subtask 1</span>
+					</div> -->
+				</div>
+				
+				
+			</div>
+		</div>
+	<!-- </form>  -->
+	<div class="taskAddedToBoard" id="taskCreatedIndication">
+		<div class="taskAddedToBoardContainer">
+			<span>Task added to board</span>
+			<img src="./assets/img/img_board_w.png" />
+		</div>
+	</div>    
+
+
+
     `;
 }
 
@@ -1054,8 +1111,6 @@ function addContactToTaskForceWithCheckBox(contact) {
 	addRemoveToggleForTaskForce(addedToTaskForce, contact, indexOfMemberOfTaskForce);
 	addedToTaskForce = !addedToTaskForce;
 	coworkersToAssignTo[contact].check = addedToTaskForce;
-	console.log(taskForce.length);
-	console.table(taskForce);
 }
 
 function generateAssignContactListForDropDownMenu(firstName, lastName, contact) {
@@ -1102,61 +1157,7 @@ function checkStatusToFalse() {
 	}
 }
 
-function clearTaskForce() {
-	checkStatusToFalse();
-	taskForce = [];
-	enableDisableAssignList();
-	console.table(taskForce.length);
-	renderBadgesMemberOfTaskForce();
-	assignListStatus = true;
-	enableDisableAssignList();
-}
-
-function getInitials() {}
-
-/* 
-!28.12.2022 Render Buttons with initials
-*/
-
-function renderBadgesMemberOfTaskForce() {
-	let badgeContainer = document.getElementById('badgesTaskForce');
-	badgeContainer.innerHTML = '';
-	for (
-		let memberOfTaskForce = 0;
-		memberOfTaskForce < taskForce.length;
-		memberOfTaskForce++
-	) {
-		const initialFirstName = taskForce[memberOfTaskForce].firstName.charAt(0);
-		const initialLastName = taskForce[memberOfTaskForce].lastName.charAt(0);
-		badgeContainer.innerHTML += generateBadgesTaskForceHtml(
-			memberOfTaskForce,
-			initialFirstName,
-			initialLastName
-		);
-	}
-}
-
-function generateBadgesTaskForceHtml(
-	memberOfTaskForce,
-	initialFirstName,
-	initialLastName
-) {
-	return /*html*/ `
-		<div id="${memberOfTaskForce}" class="badgeTaskForce">
-			${initialFirstName}${initialLastName}
-		</div>
-	`;
-}
-
-function hideBadgesTaskForce() {
-	document.getElementById('badgesTaskForce').classList.remove('d-none');
-}
-
-function showBadgesTaskForce() {
-	document.getElementById('badgesTaskForce').classList.add('d-none');
-}
-
-let BackgroundColorForBadges = [
+let backgroundColorForBadges = [
 	'#02CF2F',
 	'#EE00D6',
 	'#0190E0',
@@ -1169,6 +1170,86 @@ let BackgroundColorForBadges = [
 	'#FF7A00',
 ];
 
-/* !
-!HIER WEITERMACHEN: ZUFÄLLIGE FARBE FÜR BADGES!
+let index;
+
+function chooseColorForTaskForceBadge(initialFirstName, initialLastName) {
+	let asciInintalFirstName = initialFirstName.charCodeAt(0);
+	let asciInintalLastName = initialLastName.charCodeAt(0);
+	let sum = asciInintalFirstName + asciInintalLastName;
+	index = sum % 10;
+}
+
+function generateBadgesTaskForceHtml(
+	memberOfTaskForce,
+	firstName,
+	lastName,
+	initialFirstName,
+	initialLastName,
+	index
+) {
+	return /*html*/ `
+		<div
+			id="${memberOfTaskForce}"
+			class="badgeTaskForce"
+			title="${firstName} ${lastName}"
+			style="background-color:${backgroundColorForBadges[index]}"
+		>
+			${initialFirstName}${initialLastName}
+		</div>
+		`;
+}
+
+function renderBadgesMemberOfTaskForce() {
+	let badgeContainer = document.getElementById('badgesTaskForce');
+	badgeContainer.innerHTML = '';
+	for (
+		let memberOfTaskForce = 0;
+		memberOfTaskForce < taskForce.length;
+		memberOfTaskForce++
+	) {
+		const initialFirstName = taskForce[memberOfTaskForce].firstName.charAt(0);
+		const initialLastName = taskForce[memberOfTaskForce].lastName.charAt(0);
+		const firstName = taskForce[memberOfTaskForce].firstName;
+		const lastName = taskForce[memberOfTaskForce].lastName;
+		chooseColorForTaskForceBadge(initialFirstName, initialLastName);
+		badgeContainer.innerHTML += generateBadgesTaskForceHtml(
+			memberOfTaskForce,
+			firstName,
+			lastName,
+			initialFirstName,
+			initialLastName,
+			index
+		);
+	}
+}
+
+function hideBadgesTaskForce() {
+	document.getElementById('badgesTaskForce').classList.remove('d-none');
+}
+
+function showBadgesTaskForce() {
+	document.getElementById('badgesTaskForce').classList.add('d-none');
+}
+
+function closeDropDownAssignTo() {
+	assignListStatus = true;
+	enableDisableAssignList();
+}
+
+function clearTaskForce() {
+	checkStatusToFalse();
+	taskForce = [];
+	enableDisableAssignList();
+	console.table(taskForce.length);
+	renderBadgesMemberOfTaskForce();
+	closeDropDownAssignTo();
+}
+
+function frontEndDeveloper() {
+	document.getElementById('selectedAssign').value = `Just frontend. Sorry! ;)`;
+}
+
+/**
+ * !NOCH ERLEDIGEN:
+FUnktion: Der, der die Task anlegt, erscheint automatisch in der Taksforce!!!!!!!
  */
