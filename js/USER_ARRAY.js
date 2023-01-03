d5fe775c9aabe14cfeee99cfd268cc7812d8c608
HassSebastian <sebastian.hass76@gmail.com>
2023-01-03 10:18:24 +0100

    user/task array add

---
+++ js/USER_ARRAY.js
@@ -0,0 +1,32 @@
+let users = [
+	{
+		name: 'Frank Jens',
+		email: 'frankjens@test.de',
+		password: '123456789',
+	},
+	{
+		name: 'Sigmunde Arafad',
+		email: 'sigmundekoehler@test.de',
+		password: '123789',
+	},
+	{
+		name: 'Gertraut Harald',
+		email: 'gertrautharald@test.de',
+		password: '123456dfgh789',
+	},
+	{
+		name: 'Klaudia Bumsbine',
+		email: 'klaudiaschoenfeld@test.de',
+		password: '123456789',
+	},
+	{
+		name: 'Peter Lustig',
+		email: 'Peterlustig@test.de',
+		password: '123456789',
+	},
+	{
+		name: 'Peter Klausen',
+		email: 'Peterlustig@test.de',
+		password: '123456789',
+	},
+];
--- test/ersterBuchstabe.js
+++ test/ersterBuchstabe.js
@@ -1,35 +1,3 @@
-let users = [
-	{
-		name: 'Frank Jens',
-		email: 'frankjens@test.de',
-		password: '123456789',
-	},
-	{
-		name: 'Sigmunde Arafad',
-		email: 'sigmundekoehler@test.de',
-		password: '123456789',
-	},
-	{
-		name: 'Gertraut Harald',
-		email: 'gertrautharald@test.de',
-		password: '123456789',
-	},
-	{
-		name: 'Klaudia Bumsbine',
-		email: 'klaudiaschoenfeld@test.de',
-		password: '123456789',
-	},
-	{
-		name: 'Peter Lustig',
-		email: 'Peterlustig@test.de',
-		password: '123456789',
-	},
-	{
-		name: 'Peter Klausen',
-		email: 'Peterlustig@test.de',
-		password: '123456789',
-	},
-];
 
 
 let colors = [
