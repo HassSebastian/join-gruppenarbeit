237338b35a2b190433990abca7fe43fde3113004
HassSebastian <sebastian.hass76@gmail.com>
2023-01-03 14:02:07 +0100

    logIn/logOut rewrite

--- test/ersterBuchstabe.js
+++ test/ersterBuchstabe.js
@@ -16,15 +16,19 @@ let alphabetOrd = { A: [], B: [], C: [], D: [], E: [], F: [], G: [], H: [], I: [
 
 function user() {
 	for (let i = 0; i < users.length; i++) {
+		// ab hier wird der jeweils erste Buchstabe des Vor und Nachnamen separiert 
 		let vorUndZuName = users[i].name;
 		let firstLetter = vorUndZuName[0];
 		let spaceIndex = users[i].name.indexOf(' ');
 		let secondName = users[i].name.substring(spaceIndex + 1);
 		let secondLetter = secondName[0];
+
+		// ab hier wird die Farbe bestimmt. Buchstaben in asci umgewandelt. 
+		// addiert und durch 7 Farben geteilt.
+		// der Restwert ist dann die Farbe aus dem colors Array in Zeile 3
 		let asciiFirstLetter = firstLetter.charCodeAt(0);
 		let asciiSecondLetter = secondLetter.charCodeAt(0);
 		let sum = asciiFirstLetter + asciiSecondLetter;
-		
 		let result = sum % 7; 	// rersult ist dann die Farbe aus dem colors Array in Zeile 3
 
 		alphabetOrd[firstLetter].push(vorUndZuName); // hier werden die namen in das alphabetOrd array eingeordnet
