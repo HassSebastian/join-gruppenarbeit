fba6a4d9039be4837db0738b986781b2e12f769f
HassSebastian <sebastian.hass76@gmail.com>
2023-01-15 12:00:43 +0100

    add login_templates and jscom

--- js/login.js
+++ js/login.js
@@ -2,102 +2,81 @@ let rememberUser = [];
 let loggedUser = [];
 let allUsers = [];
 
+
 function initSignIn() {
 	window.location.href = './signUp.html';
 }
+
 
 function forgotPassword() {
 	window.location.href = './forgotMyP_sendMail.html';
 }
 
+
 function guestLogIn() {
 	alert('muss noch mit Gast Log in verbunden werden!');
 }
 
+
+/**
+ * If the input is correct, then do nothing, otherwise, show the error message.
+ */
 // check input formatting
 function checkCorrectInput() {
 	let email = document.getElementById('inputEmailLogin');
 	let password = document.getElementById('inputPasswordLogin');
 	let requiredEmail = document.getElementById('requiredEmailLogin');
 	let requiredPassword = document.getElementById('requiredPasswordLogin');
-
-	requiredEmail.classList.remove('requiredOn');
-	requiredPassword.classList.remove('requiredOn');
-	requiredEmail.innerHTML = `This field is required`;
-	requiredPassword.innerHTML = `This field is required`;
-
-	if (email.value.length || password.value.length) {
-		if (
-			email.value.length < 8 ||
-			!email.value.includes('@') ||
-			!email.value.includes('.') ||
-			email.value[0] === ' '
-		) {
-			requiredEmail.classList.add('requiredOn');
-		} else {
-			requiredEmail.classList.remove('requiredOn');
-		}
-		if (password.value.length == 0 || email.value[0] === ' ') {
-			requiredPassword.classList.add('requiredOn');
-		} else {
-			requiredPassword.classList.remove('requiredOn');
-		}
-		if (
-			!requiredEmail.classList.contains('requiredOn') &&
-			!requiredPassword.classList.contains('requiredOn')
-		) {
-			userLogin(email.value, password.value);
-		}
-	}
+	resetRequiredLine(email, password, requiredEmail, requiredPassword);
+	calculateCheckCorrectInput(email, password, requiredEmail, requiredPassword);
 }
 
-// user login
 
+/**
+ * If the allUsers array is empty, then display a message to the user to register, otherwise, check the
+ * user's email and password.
+ * @param email - the email the user entered
+ * @param password - the password the user entered
+ */
+// user login
 async function userLogin(email, password) {
 	await loadTask();
 	let requiredEmailLogin = document.getElementById('requiredEmailLogin');
 	let requiredPasswordLogin = document.getElementById('requiredPasswordLogin');
 	if (allUsers.length == null) {
-		requiredEmailLogin.classList.add('requiredOn');
-		requiredEmailLogin.innerHTML = `No user available. please  <b>Sign up!!</b>`;
-		requiredPasswordLogin.classList.add('requiredOn');
-		requiredPasswordLogin.innerHTML = `No user available. please  <b>Sign up!!</b>`;
+		pleaseRegister(requiredEmailLogin, requiredPasswordLogin);
 	} else {
-		let loginStatus;
-		for (let i = 0; i < allUsers.length; i++) {
-			let emailData = allUsers[i]['email'];
-			if (emailData == email) {
-				if (allUsers[i]['password'] == password) {
-					loginStatus = i;
-					break;
-				}
-			}
-		}
-		if (loginStatus) {
-			rememberMe(email, password, loginStatus);
-		} else {
-			requiredEmailLogin.classList.add('requiredOn');
-			requiredEmailLogin.innerHTML = `Email or Password do not match!!`;
-			requiredPasswordLogin.classList.add('requiredOn');
-			requiredPasswordLogin.innerHTML = `Email or Password do not match!!`;
-		}
+		statusOK(email, password, requiredEmailLogin, requiredPasswordLogin);
 	}
 }
 
+
+/**
+ * If the checkbox is checked, then the user's email and password are saved to local storage, and the
+ * user is redirected to the summary page.
+ * @param email - the email of the user
+ * @param password - the password the user entered
+ * @param loginStatus - true or false
+ */
 //  remember me checkbox check
 function rememberMe(email, password, loginStatus) {
 	let checkbox = document.getElementById('checkbox');
 	if (checkbox.checked) {
 		rememberUserExisting(email, password);
 	}
-
 	loggedUser.push(loginStatus);
 	let loggedUserAsString = JSON.stringify(loggedUser);
 	localStorage.setItem('loggedUser', loggedUserAsString);
-
 	window.location.href = './summary.html';
 }
 
+
+/**
+ * If the localStorage key 'rememberUser' is null, then call the function keyQueryNull, otherwise call
+ * the function rememberDoubleUserCheck.
+ * @param email - the email address of the user
+ * @param password - the password the user entered
+ */
 // remember me check - user existing in localstorage
 function rememberUserExisting(email, password) {
 	let keyQuery = localStorage.getItem('rememberUser');
@@ -108,42 +87,26 @@ function rememberUserExisting(email, password) {
 	}
 }
 
+
+/**
+ * It checks if the email address is already in localStorage.
+ * @param email - the email address of the user
+ * @param password - the password that the user entered
+ */
 // remember me check - double entries
 function rememberDoubleUserCheck(email, password) {
 	let doubleUserCheckAtString = localStorage.getItem('rememberUser');
 	let rememberUser = JSON.parse(doubleUserCheckAtString);
 	let valueToCheck = email;
 	let check = 0;
-	for (let i = 0; i < rememberUser.length; i++) {
-		let testValue = rememberUser[i].email;
-		if (testValue === valueToCheck) {
-			check = 1;
-			break;
-		}
-	}
-	if (check == 1) {
-		window.location.href = './summary.html';
-	} else {
-		keyQueryOne(email, password);
-	}
+	calculateRememberDoubleUserCheck(email, password, rememberUser, valueToCheck, check);
 }
 
-// remember me help fuction -null-
-function keyQueryNull(email, password) {
-	rememberUser.push({ email: email, password: password });
-	let rememberUserAtString = JSON.stringify(rememberUser);
-	localStorage.setItem('rememberUser', rememberUserAtString);
-}
 
-// remember me help function -one-
-function keyQueryOne(email, password) {
-	let rememberUserString = localStorage.getItem('rememberUser');
-	rememberUser = JSON.parse(rememberUserString);
-	rememberUser.push({ email: email, password: password });
-	let rememberUserAtString = JSON.stringify(rememberUser);
-	localStorage.setItem('rememberUser', rememberUserAtString);
-}
-
+/**
+ * If the password icon is hidden, show it and change the password input type to "password". If the
+ * password icon is not hidden, hide it and change the password input type to "text".
+ */
 function passwordShowIcon() {
 	document.getElementById('passwordLogo').classList.toggle('d-none');
 	document.getElementById('pwShowButton').classList.toggle('d-none');
@@ -154,12 +117,17 @@ function passwordShowIcon() {
 	}
 }
 
+
+/**
+ * If there is a string in localStorage with the key 'rememberUser', then parse the string into an
+ * object, push the object into an array, and then set the value of the email and password inputs to
+ * the email and password of the object.
+ */
 function logInAtLocalstorage(){
 	let rememberUserString = localStorage.getItem('rememberUser');
 	if(rememberUserString){
 	rememberUser = JSON.parse(rememberUserString);
 	rememberUser.push(rememberUser);
-
 	document.getElementById('inputEmailLogin').value=`${rememberUser[0].email}`;
 	document.getElementById('inputPasswordLogin').value=`${rememberUser[0].password}`;
 	}
---
+++ js/login_tamplates.js
@@ -0,0 +1,159 @@
+/**
+ * This function removes the class 'requiredOn' from the requiredEmail and requiredPassword elements,
+ * and sets the innerHTML of both elements to 'This field is required'.
+ * @param email - the email input field
+ * @param password - the password input field
+ * @param requiredEmail - The element that will display the error message for the email field.
+ * @param requiredPassword - The element that will display the error message for the password field.
+ */
+function resetRequiredLine(email, password, requiredEmail, requiredPassword){
+    requiredEmail.classList.remove('requiredOn');
+	requiredPassword.classList.remove('requiredOn');
+	requiredEmail.innerHTML = `This field is required`;
+	requiredPassword.innerHTML = `This field is required`;
+}
+
+
+/**
+ * If the email and password fields are not empty, check if the email is valid, if not, show the
+ * required email message, if it is, check if the password is empty, if it is, show the required
+ * password message, if it isn't, check if the required email and password messages are not showing, if
+ * they aren't, run the userLogin function.
+ * @param email - the email input field
+ * @param password - the password input field
+ * @param requiredEmail - the element that will be shown if the email is not valid
+ * @param requiredPassword - the password input field
+ */
+function calculateCheckCorrectInput(email, password, requiredEmail, requiredPassword){
+    if (email.value.length || password.value.length) {
+		if (
+			email.value.length < 8 ||
+			!email.value.includes('@') ||
+			!email.value.includes('.') ||
+			email.value[0] === ' '
+		) {
+			requiredEmail.classList.add('requiredOn');
+		} else {
+			requiredEmail.classList.remove('requiredOn');
+		}
+		if (password.value.length == 0 || email.value[0] === ' ') {
+			requiredPassword.classList.add('requiredOn');
+		} else {
+			requiredPassword.classList.remove('requiredOn');
+		}
+		if (
+			!requiredEmail.classList.contains('requiredOn') &&
+			!requiredPassword.classList.contains('requiredOn')
+		) {
+			userLogin(email.value, password.value);
+		}
+	}
+}
+
+
+/**
+ * If the user doesn't exist, then add the class 'requiredOn' to the email and password fields, and add
+ * the text 'No user available. please  Sign up!!' to the email and password fields.
+ * @param requiredEmailLogin - the element that will display the error message
+ * @param requiredPasswordLogin - the password input field
+ */
+function pleaseRegister(requiredEmailLogin, requiredPasswordLogin){
+    requiredEmailLogin.classList.add('requiredOn');
+    requiredEmailLogin.innerHTML = `No user available. please  <b>Sign up!!</b>`;
+    requiredPasswordLogin.classList.add('requiredOn');
+    requiredPasswordLogin.innerHTML = `No user available. please  <b>Sign up!!</b>`;
+}
+
+
+/**
+ * If the email and password match, then the user is logged in.
+ * @param email - the email that the user entered
+ * @param password - the password the user entered
+ * @param requiredEmailLogin - is the element that will display the error message if the email is not
+ * found in the database.
+ * @param requiredPasswordLogin - is the element that will display the error message if the password is
+ * incorrect.
+ */
+function statusOK(email, password, requiredEmailLogin, requiredPasswordLogin){
+    let loginStatus;
+    for (let i = 0; i < allUsers.length; i++) {
+        let emailData = allUsers[i]['email'];
+        if (emailData == email) {
+            if (allUsers[i]['password'] == password) {
+                loginStatus = i;
+                break;
+            }
+        }
+    }
+    if (loginStatus) {
+        rememberMe(email, password, loginStatus);
+    } else {
+        requiredEmailLogin.classList.add('requiredOn');
+        requiredEmailLogin.innerHTML = `Email or Password do not match!!`;
+        requiredPasswordLogin.classList.add('requiredOn');
+        requiredPasswordLogin.innerHTML = `Email or Password do not match!!`;
+    }
+}
+
+
+/**
+ * It checks if the email and password entered by the user is in the local storage. If it is, it
+ * redirects the user to the summary page. If it isn't, it calls the keyQueryOne function.
+ * @param email - the email address of the user
+ * @param password - the password the user entered
+ * @param rememberUser - [{email: "test@test.com", password: "test"}]
+ * @param valueToCheck - email
+ * @param check - 0
+ */
+function calculateRememberDoubleUserCheck(email, password, rememberUser, valueToCheck, check){
+    for (let i = 0; i < rememberUser.length; i++) {
+		let testValue = rememberUser[i].email;
+		if (testValue === valueToCheck) {
+			check = 1;
+			break;
+		}
+	}
+	if (check == 1) {
+		window.location.href = './summary.html';
+	} else {
+		keyQueryOne(email, password);
+	}
+}
+
+
+/**
+ * It takes two arguments, email and password, and pushes them into an array called rememberUser. 
+ * 
+ * Then it converts the array into a string and stores it in localStorage. 
+ * 
+ * The function is called when the user clicks the checkbox. 
+ * 
+ * The function is called with the email and password values from the form. 
+ * 
+ * The function is called from the checkbox's onclick event. 
+ * 
+ * The function is called from the check
+ * @param email - the email address of the user
+ * @param password - the password of the user
+ */
+// remember me help fuction -null-
+function keyQueryNull(email, password) {
+	rememberUser.push({ email: email, password: password });
+	let rememberUserAtString = JSON.stringify(rememberUser);
+	localStorage.setItem('rememberUser', rememberUserAtString);
+}
+
+
+/**
+ * It takes two arguments, email and password, and pushes them into an array called rememberUser.
+ * @param email - the email address of the user
+ * @param password - the password of the user
+ */
+// remember me help function -one-
+function keyQueryOne(email, password) {
+	let rememberUserString = localStorage.getItem('rememberUser');
+	rememberUser = JSON.parse(rememberUserString);
+	rememberUser.push({ email: email, password: password });
+	let rememberUserAtString = JSON.stringify(rememberUser);
+	localStorage.setItem('rememberUser', rememberUserAtString);
+}
