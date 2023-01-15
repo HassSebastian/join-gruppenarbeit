fbab5deb442688450d290c8721d3e9f21926ec69
HassSebastian <sebastian.hass76@gmail.com>
2023-01-15 16:16:51 +0100

    contacts_mobile progress

---
+++ test/mobile/contacts_mobile.js
@@ -0,0 +1,8 @@
+
+
+
+
+async function render_contacts_mobile() {
+    document.getElementById('mobilContent').innerHTML = '';
+    document.getElementById('mobilContent').innerHTML += showLettersHTML(alphabetLetter);
+}
