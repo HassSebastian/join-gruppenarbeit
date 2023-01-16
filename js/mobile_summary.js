52b32a70d6862b56b4c15df32703f7757d1265b1
Stefan Boskamp <stboskamp@gmail.com>
2023-01-16 02:21:34 +0100

    summary mobil base finished

--- js/mobile_summary.js
+++ js/mobile_summary.js
@@ -96,48 +96,48 @@ function generateMobilSummaryHtml(){
         </div>`;
 }
 
-function toDoHoverOn() {
-	document.getElementById('toDoImg').src = './assets/img/to_do_pen_black.png';
-	document.getElementById('toDo').classList.add('toDoHover');
-	document.getElementById('toDoAmountTasks').classList.add('toDoHoverSpanP');
-	document.getElementById('toDoAmountP').classList.add('toDoHoverSpanP');
-}
+// function toDoHoverOn() {
+// 	document.getElementById('toDoImg').src = './assets/img/to_do_pen_black.png';
+// 	document.getElementById('toDo').classList.add('toDoHover');
+// 	document.getElementById('toDoAmountTasks').classList.add('toDoHoverSpanP');
+// 	document.getElementById('toDoAmountP').classList.add('toDoHoverSpanP');
+// }
 
-function toDoHoverOff() {
-	document.getElementById('toDoImg').src = './assets/img/to_do_pen.png';
-	document.getElementById('toDo').classList.remove('toDoHover');
-	document.getElementById('toDoAmountTasks').classList.remove('toDoHoverSpanP');
-	document.getElementById('toDoAmountP').classList.remove('toDoHoverSpanP');
-}
+// function toDoHoverOff() {
+// 	document.getElementById('toDoImg').src = './assets/img/to_do_pen.png';
+// 	document.getElementById('toDo').classList.remove('toDoHover');
+// 	document.getElementById('toDoAmountTasks').classList.remove('toDoHoverSpanP');
+// 	document.getElementById('toDoAmountP').classList.remove('toDoHoverSpanP');
+// }
 
-function toDoDoneHoverOn() {
-	document.getElementById('toDoDoneImg').src = './assets/img/done_black.png';
-	document.getElementById('toDoDone').classList.add('toDoHover');
-	document.getElementById('toDoDoneAmountTasks').classList.add('toDoHoverSpanP');
-	document.getElementById('toDoDoneAmountP').classList.add('toDoHoverSpanP');
-}
+// function toDoDoneHoverOn() {
+// 	document.getElementById('toDoDoneImg').src = './assets/img/done_black.png';
+// 	document.getElementById('toDoDone').classList.add('toDoHover');
+// 	document.getElementById('toDoDoneAmountTasks').classList.add('toDoHoverSpanP');
+// 	document.getElementById('toDoDoneAmountP').classList.add('toDoHoverSpanP');
+// }
 
-function toDoDoneHoverOff() {
-	document.getElementById('toDoDoneImg').src = './assets/img/done.png';
-	document.getElementById('toDoDone').classList.remove('toDoHover');
-	document.getElementById('toDoDoneAmountTasks').classList.remove('toDoHoverSpanP');
-	document.getElementById('toDoDoneAmountP').classList.remove('toDoHoverSpanP');
-}
+// function toDoDoneHoverOff() {
+// 	document.getElementById('toDoDoneImg').src = './assets/img/done.png';
+// 	document.getElementById('toDoDone').classList.remove('toDoHover');
+// 	document.getElementById('toDoDoneAmountTasks').classList.remove('toDoHoverSpanP');
+// 	document.getElementById('toDoDoneAmountP').classList.remove('toDoHoverSpanP');
+// }
 
-function ugencySummaryHoverOn() {
-	document.getElementById('ugencySummaryAmount').classList.add('toDoHoverSpanP');
-	document.getElementById('deadlineDate').classList.add('toDoHoverSpanP');
-	document.getElementById('deadlineText').classList.add('toDoHoverSpanP');
-	document.getElementById('ugencySummaryurgent').classList.add('toDoHoverSpanP');
-}
+// function ugencySummaryHoverOn() {
+// 	document.getElementById('ugencySummaryAmount').classList.add('toDoHoverSpanP');
+// 	document.getElementById('deadlineDate').classList.add('toDoHoverSpanP');
+// 	document.getElementById('deadlineText').classList.add('toDoHoverSpanP');
+// 	document.getElementById('ugencySummaryurgent').classList.add('toDoHoverSpanP');
+// }
 
 /**
  * It removes the class 'toDoHoverSpanP' from the elements with the ids 'ugencySummaryAmount',
  * 'deadlineDate', 'deadlineText', and 'ugencySummaryurgent'.
  */
-function ugencySummaryHoverOff() {
-	document.getElementById('ugencySummaryAmount').classList.remove('toDoHoverSpanP');
-	document.getElementById('deadlineDate').classList.remove('toDoHoverSpanP');
-	document.getElementById('deadlineText').classList.remove('toDoHoverSpanP');
-	document.getElementById('ugencySummaryurgent').classList.remove('toDoHoverSpanP');
-}
+// function ugencySummaryHoverOff() {
+// 	document.getElementById('ugencySummaryAmount').classList.remove('toDoHoverSpanP');
+// 	document.getElementById('deadlineDate').classList.remove('toDoHoverSpanP');
+// 	document.getElementById('deadlineText').classList.remove('toDoHoverSpanP');
+// 	document.getElementById('ugencySummaryurgent').classList.remove('toDoHoverSpanP');
+// }
