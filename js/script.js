9439c3fb2df00220bccdb8b83325081c7c5b8dc7
Stefan Boskamp <stboskamp@gmail.com>
2023-01-17 10:54:50 +0100

    script responsiv function edit

--- js/script.js
+++ js/script.js
@@ -233,7 +233,7 @@ function myFunction(tabWidth) {
         if (!mobilView){
             content.style = 'height: auto;';
             document.getElementById('dropdownImg').style = 'left: calc(100vw - 170px ) !important;';
-            document.getElementById('assignDropDownImg').style = 'left: calc(100vw - 170px ) !important;';
+            document.getElementById('assignDropDownImg').style = 'left: calc(100vw - 170px ) !important; display: flex !important; position: absolute !important;';
             document.getElementById('addTaskNewCatBtn').style = 'left: calc(100vw - 221px ) !important;';
             document.getElementById('CatListDropdown').style = 'width: calc(100vw - 142px) !important;';
             document.getElementById('dropdown2').style = 'width: calc(100vw - 142px) !important;';
@@ -247,7 +247,7 @@ function myFunction(tabWidth) {
         if (!mobilView){
             content.style = 'height: 1024px';
             document.getElementById('dropdownImg').style = 'left: 392px !important;';
-            document.getElementById('assignDropDownImg').style = 'left: 392px !important;';
+            document.getElementById('assignDropDownImg').style = 'left: 392px !important; display: flex !important; position: absolute !important;';
             document.getElementById('addTaskNewCatBtn').style = 'left: 341px !important;';
             document.getElementById('CatListDropdown').style = 'width: 424px !important;';
             document.getElementById('dropdown2').style = 'width: 424px !important;';
