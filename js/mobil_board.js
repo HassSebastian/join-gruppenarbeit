1fc22d33fbbdac6e6406eaafb8bee9e79a458af3
Stefan Boskamp <stboskamp@gmail.com>
2023-01-18 04:36:24 +0100

    board mobil started

--- js/mobil_board.js
+++ js/mobil_board.js
@@ -1,15 +1,35 @@
-async function initBoard() {
-    await includeHTML();
+async function initMobilBoard() {
+    // await includeHTML();
     await renderMobileBoard();
-    selectedMenuBtnId = 0;
+    // selectedMenuBtnId = 0;
     selectedMenuButton(2);
-    await loadTask();
-    await createWorkStatusArrays();
-    renderAllCards();
-    loadContributorsLetter();
+    // await loadTask();
+    // await createWorkStatusArrays();
+    // renderAllCards();
+    // loadContributorsLetter();
 }
 
 
-function renderMobileBoard(){
-    
+async function renderMobileBoard(){
+    document.getElementById('mobilContent').innerHTML = '';
+	document.getElementById('mobilContent').innerHTML = /*html*/ `
+        <span class="kanbanProjectManagementTool">
+            Kanban Project Management Tool
+        </span>
+        <div class="headlineContainerBoardMob">
+            <span Class="headlineMob">
+                Board
+            </span>
+        </div>
+        <div class="boardSearchMobil">
+            <div class='frame'>
+                <div class='frame121'>
+                    <input type="text" class='frame108'>
+                    <div class='frame122'>
+                        <img src="../assets/img/board_mobil_lupe.png" alt="">
+                    </div>
+                </div>   
+            </div>
+        </div>
+        `;
 }
