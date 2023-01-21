d22f1cb0fa50cb433b89e18ade4acdef6890635d
GreenfieldC <101995422+GreenfieldC@users.noreply.github.com>
2023-01-21 10:23:43 +0100

    just formating

--- js/board_part_1.js
+++ js/board_part_1.js
@@ -6,22 +6,14 @@ let workStatus3Array = [];
 let boardEditedPrio = '';
 let currentDraggedElement;
 let showContributorsPrioIcons = { M: './assets/img/medium.png', U: './assets/img/urgent.png', L: './assets/img/low.png' };
-let categoryBackgroundColors = [
-    '#8aa4ff',
-    '#ff0000',
-    '#2ad300',
-    '#ff8a00',
-    '#e200be',
-    '#0038ff'
-];
+let categoryBackgroundColors = ['#8aa4ff', '#ff0000', '#2ad300', '#ff8a00', '#e200be', '#0038ff'];
 let prioColorAndUrlArray = [
-    {
-        'urgent': ['#FF3D00', './assets/img/urgent_white.png'],
-        'medium': ['#FFA800', './assets/img/medium_white.png'],
-        'low': ['#7AE229', './assets/img/low_white.png'],
-    }
+	{
+		urgent: ['#FF3D00', './assets/img/urgent_white.png'],
+		medium: ['#FFA800', './assets/img/medium_white.png'],
+		low: ['#7AE229', './assets/img/low_white.png'],
+	},
 ];
-
 
 /**
  * The function initBoard() is an async function that calls the async functions includeHTML(),
@@ -29,79 +21,69 @@ let prioColorAndUrlArray = [
  * renderAllCards().
  */
 async function initBoard() {
-    // setURL('https://gruppe-407.developerakademie.net/smallest_backend_ever');
-    // await includeHTML();
-    await renderBoard();
-    // selectedMenuBtnId = 0;
-    selectedMenuButton(2);
-    await loadTask();
-    await createWorkStatusArrays();
-    renderAllCards();
-    loadContributorsLetter();
+	// setURL('https://gruppe-407.developerakademie.net/smallest_backend_ever');
+	// await includeHTML();
+	await renderBoard();
+	// selectedMenuBtnId = 0;
+	selectedMenuButton(2);
+	await loadTask();
+	await createWorkStatusArrays();
+	renderAllCards();
+	loadContributorsLetter();
 }
-
 
 /**
  * this function render the basic board html structur into the HTML template div content.
  */
 async function renderBoard() {
-    document.getElementById('content').innerHTML = '';
-    document.getElementById('content').innerHTML += boardHtml();
+	document.getElementById('content').innerHTML = '';
+	document.getElementById('content').innerHTML += boardHtml();
 }
-
 
 /**
  * This function clear all Arrays related to the workstatus.
  * The workstatus is releted to the progress of processing of the task.
  */
 function resetWorkStatusArrays() {
-    workStatus0Array = [];
-    workStatus1Array = [];
-    workStatus2Array = [];
-    workStatus3Array = [];
-    workStatusArray = [
-        workStatus0Array,
-        workStatus1Array,
-        workStatus2Array,
-        workStatus3Array
-    ];
+	workStatus0Array = [];
+	workStatus1Array = [];
+	workStatus2Array = [];
+	workStatus3Array = [];
+	workStatusArray = [workStatus0Array, workStatus1Array, workStatus2Array, workStatus3Array];
 }
-
 
 /**
  * For each element in the joinTaskArray, if the element's workFlowStatus is equal to the index, then
  * createWorkStatusArrayData(index, i)
  */
 async function createWorkStatusArrays() {
-    resetWorkStatusArrays();
-    for (let index = 0; index < 4; index++) {
-        for (let i = 0; i < joinTaskArray.length; i++) {
-            let taskWorkStatus = joinTaskArray[i]['workFlowStatus'];
-            if (taskWorkStatus == index) {
-                createWorkStatusArrayData(index, i)
-            }
-        }
-    }
+	resetWorkStatusArrays();
+	for (let index = 0; index < 4; index++) {
+		for (let i = 0; i < joinTaskArray.length; i++) {
+			let taskWorkStatus = joinTaskArray[i]['workFlowStatus'];
+			if (taskWorkStatus == index) {
+				createWorkStatusArrayData(index, i);
+			}
+		}
+	}
 }
-
 
 /**
  * It takes the data from the joinTaskArray and creates a new array called workStatusArray.
  * @param index - the index of the array that the json will be pushed to
  * @param taskIndex - the index of the task in the joinTaskArray
  */
-function createWorkStatusArrayData(index, taskIndex){
-    let cardTitle = joinTaskArray[taskIndex]['title'];
-    let cardDescription = joinTaskArray[taskIndex]['descripten'];
-    let cardCatColor = joinTaskArray[taskIndex]['catColor'];
-    let cardCategory = joinTaskArray[taskIndex]['category'];
-    let cardPrio = joinTaskArray[taskIndex]['prio'];
-    let subTasks = joinTaskArray[taskIndex]['subTasks'];
-    let assignedToList = joinTaskArray[taskIndex]['assignedTo'];
-    let wJson = createWorkStatusJson(cardTitle, cardDescription, cardCatColor, cardCategory, cardPrio, subTasks, taskIndex, assignedToList);
-    workStatusArray[index].push(wJson);
+function createWorkStatusArrayData(index, taskIndex) {
+	let cardTitle = joinTaskArray[taskIndex]['title'];
+	let cardDescription = joinTaskArray[taskIndex]['descripten'];
+	let cardCatColor = joinTaskArray[taskIndex]['catColor'];
+	let cardCategory = joinTaskArray[taskIndex]['category'];
+	let cardPrio = joinTaskArray[taskIndex]['prio'];
+	let subTasks = joinTaskArray[taskIndex]['subTasks'];
+	let assignedToList = joinTaskArray[taskIndex]['assignedTo'];
+	let wJson = createWorkStatusJson(cardTitle, cardDescription, cardCatColor, cardCategory, cardPrio, subTasks, taskIndex, assignedToList);
+	workStatusArray[index].push(wJson);
 }
-
 
 /**
  * It takes in a bunch of parameters and returns a JSON object.
@@ -125,30 +107,28 @@ function createWorkStatusArrayData(index, taskIndex){
  * assignedTo
  */
 function createWorkStatusJson(cardTitle, cardDescription, cardCatColor, cardCategory, cardPrio, subTasks, taskIndex, assignedToList) {
-    return {
-        'cardTitle': cardTitle,
-        'cardDescription': cardDescription,
-        'cardCatColor': cardCatColor,
-        'cardCategory': cardCategory,
-        'cardPrio': cardPrio,
-        'subTasks': subTasks,
-        'taskIndex': taskIndex,
-        'assignedTo': assignedToList,
-    };
+	return {
+		cardTitle: cardTitle,
+		cardDescription: cardDescription,
+		cardCatColor: cardCatColor,
+		cardCategory: cardCategory,
+		cardPrio: cardPrio,
+		subTasks: subTasks,
+		taskIndex: taskIndex,
+		assignedTo: assignedToList,
+	};
 }
-
 
 /**
  * This function renders all the cards in the Kanban board.
  */
 async function renderAllCards() {
-    renderToDoCards();
-    renderInProgressCards();
-    renderAwaitingFeedbackCards();
-    renderDoneCards();
-    renderAssignTo();
+	renderToDoCards();
+	renderInProgressCards();
+	renderAwaitingFeedbackCards();
+	renderDoneCards();
+	renderAssignTo();
 }
-
 
 /**
  * It takes two numbers, divides the first by the second, and multiplies the result by 100
@@ -157,38 +137,34 @@ async function renderAllCards() {
  * @returns The percentage of the number in relation to the total.
  */
 function calculatePercentage(number, total) {
-    return (number / total) * 100;
+	return (number / total) * 100;
 }
-
-
 
 /**
  * It loops through an array of objects, and for each object, it adds a card to the DOM.
  */
 async function renderToDoCards() {
-    document.getElementById('toDoDiv').innerHTML = '';
-    for (let i = 0; i < workStatus0Array.length; i++) {
-        document.getElementById('toDoDiv').innerHTML += toDoCardHtml(i);
-        let taskIndex = workStatus0Array[i]['taskIndex'];
-        showContributorsPrioIcon(taskIndex);
-    }
-    setCategoryBackgroundColorForWorkStatus0();
+	document.getElementById('toDoDiv').innerHTML = '';
+	for (let i = 0; i < workStatus0Array.length; i++) {
+		document.getElementById('toDoDiv').innerHTML += toDoCardHtml(i);
+		let taskIndex = workStatus0Array[i]['taskIndex'];
+		showContributorsPrioIcon(taskIndex);
+	}
+	setCategoryBackgroundColorForWorkStatus0();
 }
-
 
 /**
  * For each array in the workStatusArray, for each object in the array, get the taskIndex property of
  * the object and pass it to the renderAssignToHtml function.
  */
 function renderAssignTo() {
-    for (let i = 0; i < workStatusArray.length; i++) {
-        for (let index = 0; index < workStatusArray[i].length; index++) {
-            let taskIndex = workStatusArray[i][index]['taskIndex'];
-            renderAssignToHtml(taskIndex);
-        }
-    }
+	for (let i = 0; i < workStatusArray.length; i++) {
+		for (let index = 0; index < workStatusArray[i].length; index++) {
+			let taskIndex = workStatusArray[i][index]['taskIndex'];
+			renderAssignToHtml(taskIndex);
+		}
+	}
 }
-
 
 /**
  * The function determindSubTasksDone() takes two arguments, the first is the index of the
@@ -199,193 +175,177 @@ function renderAssignTo() {
  * @returns The amount of subTasks that are done.
  */
 function determindSubTasksDone(arrayIndex, workStatusArrayNo) {
-    let doneAmount = 0;
-    for (let i = 0; i < workStatusArray[workStatusArrayNo][arrayIndex]['subTasks'].length; i++) {
-        let subTaskStatus = workStatusArray[workStatusArrayNo][arrayIndex]['subTasks'][i]['subtaskStatus'];
-        if (subTaskStatus) {
-            doneAmount += 1;
-        }
-    }
-    return doneAmount;
+	let doneAmount = 0;
+	for (let i = 0; i < workStatusArray[workStatusArrayNo][arrayIndex]['subTasks'].length; i++) {
+		let subTaskStatus = workStatusArray[workStatusArrayNo][arrayIndex]['subTasks'][i]['subtaskStatus'];
+		if (subTaskStatus) {
+			doneAmount += 1;
+		}
+	}
+	return doneAmount;
 }
-
 
 /**
  * this function set the backgroundcolor of the catogory in the todo task card.
  */
 function setCategoryBackgroundColorForWorkStatus0() {
-    for (let i = 0; i < workStatus0Array.length; i++) {
-        let cardCatColor = workStatus0Array[i]['cardCatColor'];
-        let catBackground = categoryBackgroundColors[cardCatColor];
-        document.getElementById(`toDoCardCat${i}`).style = `background-color: ${catBackground};`;
-    }
+	for (let i = 0; i < workStatus0Array.length; i++) {
+		let cardCatColor = workStatus0Array[i]['cardCatColor'];
+		let catBackground = categoryBackgroundColors[cardCatColor];
+		document.getElementById(`toDoCardCat${i}`).style = `background-color: ${catBackground};`;
+	}
 }
-
 
 /**
  * this function render all in progress cards.
  */
 function renderInProgressCards() {
-    document.getElementById('progressDiv').innerHTML = '';
-    for (let i = 0; i < workStatus1Array.length; i++) {
-        document.getElementById('progressDiv').innerHTML += inProgressHtml(i);
-        let taskIndex = workStatus1Array[i]['taskIndex'];
-        showContributorsPrioIcon(taskIndex);
-    }
-    setCategoryBackgroundColorForWorkStatus1();
+	document.getElementById('progressDiv').innerHTML = '';
+	for (let i = 0; i < workStatus1Array.length; i++) {
+		document.getElementById('progressDiv').innerHTML += inProgressHtml(i);
+		let taskIndex = workStatus1Array[i]['taskIndex'];
+		showContributorsPrioIcon(taskIndex);
+	}
+	setCategoryBackgroundColorForWorkStatus1();
 }
-
 
 /**
  * this function set the backgroundcolor of the catogory in the in progress task card.
  */
 function setCategoryBackgroundColorForWorkStatus1() {
-    for (let i = 0; i < workStatus1Array.length; i++) {
-        let cardCatColor = workStatus1Array[i]['cardCatColor'];
-        let catBackground = categoryBackgroundColors[cardCatColor];
-        document.getElementById(`progressCard${i}`).style = `background-color: ${catBackground};`;
-    }
+	for (let i = 0; i < workStatus1Array.length; i++) {
+		let cardCatColor = workStatus1Array[i]['cardCatColor'];
+		let catBackground = categoryBackgroundColors[cardCatColor];
+		document.getElementById(`progressCard${i}`).style = `background-color: ${catBackground};`;
+	}
 }
-
 
 /**
  * this function render all awaiting feedback cards.
  */
 function renderAwaitingFeedbackCards() {
-    document.getElementById('awaitingDiv').innerHTML = '';
-    for (let i = 0; i < workStatus2Array.length; i++) {
-        document.getElementById('awaitingDiv').innerHTML += awaitingFeedbackHtml(i);
-        let taskIndex = workStatus2Array[i]['taskIndex'];
-        showContributorsPrioIcon(taskIndex);
-    }
-    setCategoryBackgroundColorForWorkStatus2();
+	document.getElementById('awaitingDiv').innerHTML = '';
+	for (let i = 0; i < workStatus2Array.length; i++) {
+		document.getElementById('awaitingDiv').innerHTML += awaitingFeedbackHtml(i);
+		let taskIndex = workStatus2Array[i]['taskIndex'];
+		showContributorsPrioIcon(taskIndex);
+	}
+	setCategoryBackgroundColorForWorkStatus2();
 }
-
 
 /**
  * this function set the backgroundcolor of the catogory in the awaiting feedback task card.
  */
 function setCategoryBackgroundColorForWorkStatus2() {
-    for (let i = 0; i < workStatus2Array.length; i++) {
-        let cardCatColor = workStatus2Array[i]['cardCatColor'];
-        let catBackground = categoryBackgroundColors[cardCatColor];
-        document.getElementById(`feedbackCard${i}`).style = `background-color: ${catBackground};`;
-    }
+	for (let i = 0; i < workStatus2Array.length; i++) {
+		let cardCatColor = workStatus2Array[i]['cardCatColor'];
+		let catBackground = categoryBackgroundColors[cardCatColor];
+		document.getElementById(`feedbackCard${i}`).style = `background-color: ${catBackground};`;
+	}
 }
-
 
 /**
  * this function render all done cards.
  */
 function renderDoneCards() {
-    document.getElementById('doneDiv').innerHTML = '';
-    for (let i = 0; i < workStatus3Array.length; i++) {
-        document.getElementById('doneDiv').innerHTML += doneHtml(i);
-        let taskIndex = workStatus3Array[i]['taskIndex'];
-        showContributorsPrioIcon(taskIndex);
-    }
-    setCategoryBackgroundColorForWorkStatus3();
+	document.getElementById('doneDiv').innerHTML = '';
+	for (let i = 0; i < workStatus3Array.length; i++) {
+		document.getElementById('doneDiv').innerHTML += doneHtml(i);
+		let taskIndex = workStatus3Array[i]['taskIndex'];
+		showContributorsPrioIcon(taskIndex);
+	}
+	setCategoryBackgroundColorForWorkStatus3();
 }
-
 
 /**
  * When the user scrolls down the page, the height of the drop areas will increase to match the height
  * of the page.
  */
 function changeHeightDropArea() {
-    const elmnt = document.getElementById('canbanBoard');
-    let y = elmnt.scrollTop;
-    let newHeight = y + 600;
-    document.getElementById('dropArea0').style = `height: ${newHeight}px;`;
-    document.getElementById('dropArea1').style = `height: ${newHeight}px;`;
-    document.getElementById('dropArea2').style = `height: ${newHeight}px;`;
-    document.getElementById('dropArea3').style = `height: ${newHeight}px;`;
+	const elmnt = document.getElementById('canbanBoard');
+	let y = elmnt.scrollTop;
+	let newHeight = y + 600;
+	document.getElementById('dropArea0').style = `height: ${newHeight}px;`;
+	document.getElementById('dropArea1').style = `height: ${newHeight}px;`;
+	document.getElementById('dropArea2').style = `height: ${newHeight}px;`;
+	document.getElementById('dropArea3').style = `height: ${newHeight}px;`;
 }
-
 
 /**
  * this function set the backgroundcolor of the catogory in the done task card.
  */
 function setCategoryBackgroundColorForWorkStatus3() {
-    for (let i = 0; i < workStatus3Array.length; i++) {
-        let cardCatColor = workStatus3Array[i]['cardCatColor'];
-        let catBackground = categoryBackgroundColors[cardCatColor];
-        document.getElementById(`doneCard${i}`).style = `background-color: ${catBackground} !important;`;
-    }
+	for (let i = 0; i < workStatus3Array.length; i++) {
+		let cardCatColor = workStatus3Array[i]['cardCatColor'];
+		let catBackground = categoryBackgroundColors[cardCatColor];
+		document.getElementById(`doneCard${i}`).style = `background-color: ${catBackground} !important;`;
+	}
 }
-
 
 /**
  * It takes the priority of a task and displays the corresponding icon.
  * @param taskIndex - The index of the task in the array.
  */
 function showContributorsPrioIcon(taskIndex) {
-    for (let a in showContributorsPrioIcons) {
-        let prio = joinTaskArray[taskIndex].prio[0];
-        if (a = prio) {
-            document.getElementById(`contributorsPrioIcon${taskIndex}`).src = `${showContributorsPrioIcons[a]}`;
-            break;
-        }
-    }
+	for (let a in showContributorsPrioIcons) {
+		let prio = joinTaskArray[taskIndex].prio[0];
+		if ((a = prio)) {
+			document.getElementById(`contributorsPrioIcon${taskIndex}`).src = `${showContributorsPrioIcons[a]}`;
+			break;
+		}
+	}
 }
-
 
 /**
  * thie function cache the id of the dragged element.
  * @param {string} id - the id is the id of the dragged element.
  */
 function startDrag(id) {
-    currentDraggedElement = id;
+	currentDraggedElement = id;
 }
-
 
 /**
  * this function allows to drop an draggable element.
- * @param {event} ev 
+ * @param {event} ev
  */
 function allowDrop(ev) {
-    ev.preventDefault();
+	ev.preventDefault();
 }
 
-
-function moveTo(area){
-    let doneBarDraggedElement = document.getElementById(`doneBar${currentDraggedElement}`);
-    let doneBarOuterDraggedElement = document.getElementById(`doneBarOuter${currentDraggedElement}`);
-    let doneBarWidth = doneBarDraggedElement.offsetWidth;
-    let doneBarOuterWidth = doneBarOuterDraggedElement.offsetWidth;
-    let workFlowStatusDraggedElement = joinTaskArray[currentDraggedElement]['workFlowStatus'];
-    if ((doneBarWidth == doneBarOuterWidth && workFlowStatusDraggedElement >= 1) || (workFlowStatusDraggedElement < 1 && area < 2)){
-        moveToNewArea(area);
-    }
-    if(area < workFlowStatusDraggedElement){
-        moveToNewArea(area);
-    }
+function moveTo(area) {
+	let doneBarDraggedElement = document.getElementById(`doneBar${currentDraggedElement}`);
+	let doneBarOuterDraggedElement = document.getElementById(`doneBarOuter${currentDraggedElement}`);
+	let doneBarWidth = doneBarDraggedElement.offsetWidth;
+	let doneBarOuterWidth = doneBarOuterDraggedElement.offsetWidth;
+	let workFlowStatusDraggedElement = joinTaskArray[currentDraggedElement]['workFlowStatus'];
+	if ((doneBarWidth == doneBarOuterWidth && workFlowStatusDraggedElement >= 1) || (workFlowStatusDraggedElement < 1 && area < 2)) {
+		moveToNewArea(area);
+	}
+	if (area < workFlowStatusDraggedElement) {
+		moveToNewArea(area);
+	}
 }
 
-
-async function moveToNewArea(area){
-    joinTaskArray[currentDraggedElement]['workFlowStatus'] = area;
-    await saveTask();
-    await createWorkStatusArrays();
-    await renderAllCards();
-    searchAfterPopup();
+async function moveToNewArea(area) {
+	joinTaskArray[currentDraggedElement]['workFlowStatus'] = area;
+	await saveTask();
+	await createWorkStatusArrays();
+	await renderAllCards();
+	searchAfterPopup();
 }
-
 
 /**
  * this function change the backgroundcolor of the droparea, if a dragged element is over it.
  * @param {string} id - is the id of the element where the dragged element is over it.
  */
 function highlight(id) {
-    document.getElementById(id).classList.add('drag-area-highlight');
+	document.getElementById(id).classList.add('drag-area-highlight');
 }
-
 
 /**
  * this function reset the backgroundcolor settings during hovering of thedragged element.
  * @param {string} id - is the id of the element where the dragged element is dropped.
  */
 function removeHighlight(id) {
-    document.getElementById(id).classList.remove('drag-area-highlight');
+	document.getElementById(id).classList.remove('drag-area-highlight');
 }
-
