4d8cba3c4df6dc27dbc33369b1f8523997dfc2bd
HassSebastian <sebastian.hass76@gmail.com>
2023-01-24 17:21:48 +0100

    mobile bug fix

--- js/mobile_login.js
+++ js/mobile_login.js
@@ -1,9 +1,6 @@
 async function initLoginMob(){
 	setURL('https://gruppe-407.developerakademie.net/smallest_backend_ever');
 	await loadTask();
-
-
-
 }
 
 
@@ -20,4 +17,5 @@ function backToLogInMob(){
     document.getElementById('logInMasterContainerMob').classList.remove('d-none');
     document.getElementById('signInMasterContainerMob').classList.add('d-none');
     document.getElementById('notAJoinUserContainerMob').classList.remove('d-none');
+    document.getElementById('contactSucc').classList.add('d-none');
 }
