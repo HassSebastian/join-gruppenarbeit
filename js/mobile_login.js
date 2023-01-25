15d730d2b23436f40739485f6133d8ee35f2bbe2
HassSebastian <sebastian.hass76@gmail.com>
2023-01-25 03:54:42 +0100

    mobile login forgot pw progress

--- js/mobile_login.js
+++ js/mobile_login.js
@@ -1,17 +1,17 @@
-async function initLoginMob(){
-	setURL('https://gruppe-407.developerakademie.net/smallest_backend_ever');
-	await loadTask();
+async function initLoginMob() {
+    setURL('https://gruppe-407.developerakademie.net/smallest_backend_ever');
+    await loadTask();
 }
 
 
-function notAJoinUserButtonMob(){
+function notAJoinUserButtonMob() {
     document.getElementById('logInMasterContainerMob').classList.add('d-none');
     document.getElementById('signInMasterContainerMob').classList.remove('d-none');
     document.getElementById('notAJoinUserContainerMob').classList.add('d-none');
 }
 
 
-function backToLogInMob(){
+function backToLogInMob() {
     document.getElementById('logInMasterContainerMob').classList.remove('d-none');
     document.getElementById('signInMasterContainerMob').classList.add('d-none');
     document.getElementById('notAJoinUserContainerMob').classList.remove('d-none');
@@ -20,12 +20,30 @@ function backToLogInMob(){
 }
 
 
-function showForgotPasswordMob(){
-document.getElementById('logInMasterContainerMob').classList.add('d-none');
-document.getElementById('notAJoinUserContainerMob').classList.add('d-none');
-document.getElementById('forgotPWMasterContainerMob').classList.remove('d-none');
+function showForgotPasswordMob() {
+    document.getElementById('logInMasterContainerMob').classList.add('d-none');
+    document.getElementById('notAJoinUserContainerMob').classList.add('d-none');
+    document.getElementById('forgotPWMasterContainerMob').classList.remove('d-none');
 }
 
-function sendMailButton(){
-document.getElementById('sentMassageDoneMaserContainerMob').classList.add('sentMassageDoneMaserContainerMobSlide');
+function sendMailButton() {
+    document.getElementById('sentMassageDoneMaserContainerMob').classList.add('sentMassageDoneMaserContainerMobSlide');
+    setTimeout(showPasswordResetCard, 3000);
+}
+
+function showPasswordResetCard(){
+    document.getElementById('sentMassageDoneMaserContainerMob').classList.add('d-none');
+    document.getElementById('forgotPWMasterContainerMob').classList.add('d-none');
+    document.getElementById('resetPWMasterContainerMob').classList.remove('d-none');
+}
+
+function backToSignInMob() {
+    document.getElementById('resetPWMasterContainerMob').classList.add('d-none');
+    document.getElementById('signInMasterContainerMob').classList.remove('d-none');
+    document.getElementById('pwResetContainerMob').classList.remove('pwResetContainerMobSlide');
+}
+
+function resetbuttonContainerMob(){
+    document.getElementById('pwResetContainerMob').classList.add('pwResetContainerMobSlide');
+    setTimeout(backToSignInMob, 3000);
 }
