ec901c5552e9210e627bce873325ee6e116fcddf
Stefan Boskamp <stboskamp@gmail.com>
2023-01-30 17:33:59 +0100

    startsequenz redisigned

--- js/script.js
+++ js/script.js
@@ -41,13 +41,90 @@ let colorUserIndex = ['#02CF2F', '#EE00D6', '#0190E0', '#FF7200', '#FF2500', '#A
 let selectedMenuBtnId;
 let includeAttribute = 'w3-include-html';
 
-// https://gruppe-407.developerakademie.net/smallest_backend_ever
+
+// index.html start functions end edit by Bossi 30.01
+function loadStartDisplay(){
+    if (window.innerWidth > 768){
+        document.getElementById('page1').classList.remove('d-none');
+        setTimeout(initPage1, 300);
+    }
+    if (window.innerWidth <= 768){
+        console.log('mobile')
+        document.getElementById('pageOneMob').classList.remove('d-none');
+        document.getElementById('mobilMasterContainer').classList.remove('d-none');
+        setTimeout(initLoginMob, 300);
+    }
+}
+
+
+async function loadApplicableSummary(){
+    if (window.innerWidth > 768){
+        document.getElementById('mobilSummary').classList.add('d-none');
+        document.getElementById('desktopSummary').classList.remove('d-none');
+        await deactivatMobil();
+        init();
+    }
+    if (window.innerWidth <= 768){
+        document.getElementById('desktopSummary').classList.add('d-none');
+        document.getElementById('mobilSummary').classList.remove('d-none');
+        await deactivatDesktop();
+        initMobilSummary();
+    }
+}
+
+
+let stylesheetDesktopDeactivationList =[
+	'stylesheetMobilTemplates',
+	'stylesheetMobilContacts',
+	'stylesheetMobilAddContacts',
+	'stylesheetSummaryMobil',
+	'stylesheetHelpMobil',
+];
+
+let jsDesktopDeactivationList =[
+	'jsMobilSummary',
+	'jsMobilAddTask',
+	'jsMobilBoard',
+	'jsMobilContacts',
+	'jsHelpMobil',
+	'legalNoticeMobil',
+]
+
+let stylesheetMobilDeactivationList = [
+    'stylesheetsummary',
+    'stylesheetAddTask',
+    'stylesheetBoard',
+    'stylesheetContacts',
+    'stylesheetResponsiv',
+    'stylesheetHelp'  
+]
+
+let jsMobilDeactivationList = [
+    'jsHelp',
+]
+
+async function deactivatMobil(){
+	stylesheetDesktopDeactivationList.forEach(stylesheet => {
+		document.getElementById(stylesheet).disabled = true;
+	});
+	jsDesktopDeactivationList.forEach(script => {
+		document.getElementById(script).disabled = true;
+	});
+}
+
+async function deactivatDesktop(){
+	stylesheetMobilDeactivationList.forEach(stylesheet => {
+		document.getElementById(stylesheet).disabled = true;
+	});
+	jsMobilDeactivationList.forEach(script => {
+		document.getElementById(script).disabled = true;
+	});
+}
+// index.html start functions end edit by Bossi 30.01
 
 async function init() {
 	await includeHTML();
 	document.getElementById('stylesheetBoardMobil').disabled = true;
-	// setURL('https://developerakademie.net/smallest_backend_ever');
-	// selectedMenuButton(1);
 	initSummary();
 }
 
