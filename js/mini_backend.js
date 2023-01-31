38b5fd8f79a91e6d3d8922aa5705228ae8d4c7e6
GreenfieldC <101995422+GreenfieldC@users.noreply.github.com>
2023-01-31 15:08:16 +0100

    guestlogged in remove from backend

--- js/mini_backend.js
+++ js/mini_backend.js
@@ -116,12 +116,10 @@ function testSetUser() {
 }
 
 async function saveTask() {
-	if (!guestLoggedIn) {
-		await fillDatabaseData();
-		setURL('https://gruppe-407.developerakademie.net/smallest_backend_ever');
-		// localStorage.setItem('joinTaskArray', JSON.stringify(joinTaskArray));
-		backend.setItem('database', JSON.stringify(database));
-	}
+	await fillDatabaseData();
+	setURL('https://gruppe-407.developerakademie.net/smallest_backend_ever');
+	// localStorage.setItem('joinTaskArray', JSON.stringify(joinTaskArray));
+	backend.setItem('database', JSON.stringify(database));
 }
 
 async function loadTask() {
