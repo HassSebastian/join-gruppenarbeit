4384dbb307cd2aa518a568177c2cda4ef6beda7a
GreenfieldC <101995422+GreenfieldC@users.noreply.github.com>
2023-02-03 21:05:28 +0100

    responsive mobile summary done

--- js/mobile_summary.js
+++ js/mobile_summary.js
@@ -49,70 +49,72 @@ async function renderMobilSummary(
 
 function generateMobilSummaryHtml() {
 	return /*html*/ `
-        <span class="kanbanProjectManagementTool">
+    <div class="summaryFloatingMasterContainer">
+        <div class="summaryHeadingContainer">
+            <span class="kanbanProjectManagementTool">
             Kanban Project Management Tool
-        </span>
-        <div class="headlineContainerMob">
-            <span Class="headlineMob">
+            </span>
+            <div class="headlineContainerMob">
+                <span Class="headlineMob">
                 Summary
-            </span>
-            <span class="headlineDisMob">
-                Everything in a nutshell!
-            </span>
+                </span>
+                <span class="headlineDisMob">
+                    Everything in a nutshell!
+                </span>
+            </div>
         </div>
-        <div class="summaryFloatingMasterContainer">
-            <div class="summaryDisplayMobilContainer">
-                <div class='taskOverviewMob'>
-                    <div id='taskInBoard' class="cardsSummaryTop">
-                    <div class="containerBox" >
-                        <span id='taskInBoardAmount'>${allYourTasksAmount}</span>
-                        <p>Task in Board</p>
-                        </div>
-                    </div>
-                    <div id='taskInProgress' class="cardsSummaryTop"><span id='taskInProgressAmount'>${allYourInProgressTasksAmount}</span>
-                        <p>Task in Progress</p>
-                    </div>
-                    <div id='awaitingFeedback' class="cardsSummaryTop"><span
-                            id='awaitingFeedbackAmount'>${allYourAwaitingFeedbackTasksAmount}</span>
-                        <p>Awaiting Feedback</p>
+        <div class="summaryDisplayMobilContainer">
+            <div class='taskOverviewMob'>
+                <div id='taskInBoard' class="cardsSummaryTop">
+                <div class="containerBox" >
+                    <span id='taskInBoardAmount'>${allYourTasksAmount}</span>
+                    <p>Task in Board</p>
                     </div>
                 </div>
+                <div id='taskInProgress' class="cardsSummaryTop"><span id='taskInProgressAmount'>${allYourInProgressTasksAmount}</span>
+                    <p>Task in Progress</p>
+                </div>
+                <div id='awaitingFeedback' class="cardsSummaryTop"><span
+                        id='awaitingFeedbackAmount'>${allYourAwaitingFeedbackTasksAmount}</span>
+                    <p>Awaiting Feedback</p>
+                </div>
+            </div>
 
-                <div class='ugencySummaryMob' onmouseover="ugencySummaryHoverOn()" onmouseout="ugencySummaryHoverOff()">
-                    <div class="ugentMob">
-                        <div class='ugentImgContainerMob'>
-                            <img class='ugentImgMob' id='urgentImg' src='../../assets/img/summary_urgent.png'>
-                        </div>
-                        <div class='ugentAmountMob'>
-                            <span id='ugencySummaryAmount'>${yourUrgentTasksAmount}</span>
-                            <p id='ugencySummaryurgent'>Urgent</p>
-                        </div>
-                        <img src='../../assets/img/vertical-line2.png' class='ugentVerticalLine'>
+            <div class='ugencySummaryMob' onmouseover="ugencySummaryHoverOn()" onmouseout="ugencySummaryHoverOff()">
+                <div class="ugentMob">
+                    <div class='ugentImgContainerMob'>
+                        <img class='ugentImgMob' id='urgentImg' src='../../assets/img/summary_urgent.png'>
                     </div>
-                    <div class='deadlineDataMob'>
-                        <p id='deadlineDate'>October 16, 2022</p>
-                        <p class='deadlineText' id='deadlineText'><b>31.01.2023 Deadline</b></p>
+                    <div class='ugentAmountMob'>
+                        <span id='ugencySummaryAmount'>${yourUrgentTasksAmount}</span>
+                        <p id='ugencySummaryurgent'>Urgent</p>
+                    </div>
+                    <img src='../../assets/img/vertical-line2.png' class='ugentVerticalLine'>
+                </div>
+                <div class='deadlineDataMob'>
+                    <p id='deadlineDate'>October 16, 2022</p>
+                    <p class='deadlineText' id='deadlineText'><b>31.01.2023 Deadline</b></p>
+                </div>
+            </div>
+
+            <div class='toDoDataMob'>
+                <div class='toDo' id='toDo' onmouseover="toDoHoverOn()" onmouseout="toDoHoverOff()">
+                    <img id='toDoImg' src='../../assets/img/to_do_pen.png' alt="">
+                    <div class='toDoAmountData'>
+                        <span id='toDoAmountTasks'>${allYourToDoTasksAmount}</span>
+                        <p id='toDoAmountP'>to-Do</p>
                     </div>
                 </div>
-
-                <div class='toDoDataMob'>
-                    <div class='toDo' id='toDo' onmouseover="toDoHoverOn()" onmouseout="toDoHoverOff()">
-                        <img id='toDoImg' src='../../assets/img/to_do_pen.png' alt="">
-                        <div class='toDoAmountData'>
-                            <span id='toDoAmountTasks'>${allYourToDoTasksAmount}</span>
-                            <p id='toDoAmountP'>to-Do</p>
-                        </div>
-                    </div>
-                    <div class='toDoDone' id='toDoDone' onmouseover="toDoDoneHoverOn()" onmouseout="toDoDoneHoverOff()">
-                        <img id='toDoDoneImg' src='../../assets/img/done.png' alt="">
-                        <div class='toDoAmountData'>
-                            <span id='toDoDoneAmountTasks'>${allYourDoneTasksAmount}</span>
-                            <p id='toDoDoneAmountP'>Done</p>
-                        </div>
+                <div class='toDoDone' id='toDoDone' onmouseover="toDoDoneHoverOn()" onmouseout="toDoDoneHoverOff()">
+                    <img id='toDoDoneImg' src='../../assets/img/done.png' alt="">
+                    <div class='toDoAmountData'>
+                        <span id='toDoDoneAmountTasks'>${allYourDoneTasksAmount}</span>
+                        <p id='toDoDoneAmountP'>Done</p>
                     </div>
                 </div>
             </div>
-        </div>`;
+        </div>
+    </div>`;
 }
 
 // LogOut Overlay //
@@ -137,9 +139,8 @@ function logOutBtnMob() {
 
 function logOutMob() {
 	helloCheck = 0;
-    initLoginMob();
+	initLoginMob();
 	// window.location.href = './indexMob.html';
-
 }
 ////////////////////
 
