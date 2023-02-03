514a5284dc1afce137a26c7925bd865435591d45
GreenfieldC <101995422+GreenfieldC@users.noreply.github.com>
2023-02-03 14:47:42 +0100

    mobilResp tablets and Smartphone started

--- js/mobile_summary.js
+++ js/mobile_summary.js
@@ -1,56 +1,54 @@
 async function initMobilSummary() {
-    setURL('https://gruppe-407.developerakademie.net/smallest_backend_ever');
-    await loadTask();
-    resetCounters();
-    await loadAmountsForSummary(); // await später für server wichtig
-    await includeHTML();
-    await renderMobilSummary(
-        allYourTasksAmount,
-        allYourToDoTasksAmount,
-        allYourInProgressTasksAmount,
-        allYourAwaitingFeedbackTasksAmount,
-        allYourDoneTasksAmount,
-        yourUrgentTasksAmount
-    );
-    selectedMenuBtnId = 0;
-    helloPageMob();
-    selectedMenuButton(1);
+	setURL('https://gruppe-407.developerakademie.net/smallest_backend_ever');
+	await loadTask();
+	resetCounters();
+	await loadAmountsForSummary(); // await später für server wichtig
+	await includeHTML();
+	await renderMobilSummary(
+		allYourTasksAmount,
+		allYourToDoTasksAmount,
+		allYourInProgressTasksAmount,
+		allYourAwaitingFeedbackTasksAmount,
+		allYourDoneTasksAmount,
+		yourUrgentTasksAmount
+	);
+	selectedMenuBtnId = 0;
+	helloPageMob();
+	selectedMenuButton(1);
 
-    showDate();
-    showTime();
-    loadContributorsLetterMob();
-    logOutMasterContainerMob();
+	showDate();
+	showTime();
+	loadContributorsLetterMob();
+	logOutMasterContainerMob();
 }
 
 function loadContributorsLetterMob() {
-    let colorIndex = allUsers[loggedUser[0]].colorIndex;
-    document.getElementById('contributorsLogoHeadderMob').style = `background:${colorUserIndex[colorIndex]}`;
-    document.getElementById(
-        'contributorsLogoHeadderLettersMob'
-    ).innerHTML = `<p style='color:white'>${allUsers[loggedUser].firstSecondLetter}</p>`;
+	let colorIndex = allUsers[loggedUser[0]].colorIndex;
+	document.getElementById('contributorsLogoHeadderMob').style = `background:${colorUserIndex[colorIndex]}`;
+	document.getElementById('contributorsLogoHeadderLettersMob').innerHTML = `<p style='color:white'>${allUsers[loggedUser].firstSecondLetter}</p>`;
 }
 
 async function renderMobilSummary(
-    allYourTasksAmount,
-    allYourToDoTasksAmount,
-    allYourInProgressTasksAmount,
-    allYourAwaitingFeedbackTasksAmount,
-    allYourDoneTasksAmount,
-    yourUrgentTasksAmount
+	allYourTasksAmount,
+	allYourToDoTasksAmount,
+	allYourInProgressTasksAmount,
+	allYourAwaitingFeedbackTasksAmount,
+	allYourDoneTasksAmount,
+	yourUrgentTasksAmount
 ) {
-    document.getElementById('mobilContent').innerHTML = '';
-    document.getElementById('mobilContent').innerHTML += generateMobilSummaryHtml(
-        allYourTasksAmount,
-        allYourToDoTasksAmount,
-        allYourInProgressTasksAmount,
-        allYourAwaitingFeedbackTasksAmount,
-        allYourDoneTasksAmount,
-        yourUrgentTasksAmount
-    );
+	document.getElementById('mobilContent').innerHTML = '';
+	document.getElementById('mobilContent').innerHTML += generateMobilSummaryHtml(
+		allYourTasksAmount,
+		allYourToDoTasksAmount,
+		allYourInProgressTasksAmount,
+		allYourAwaitingFeedbackTasksAmount,
+		allYourDoneTasksAmount,
+		yourUrgentTasksAmount
+	);
 }
 
 function generateMobilSummaryHtml() {
-    return /*html*/ `
+	return /*html*/ `
         <span class="kanbanProjectManagementTool">
             Kanban Project Management Tool
         </span>
@@ -63,63 +61,64 @@ function generateMobilSummaryHtml() {
             </span>
         </div>
         <div class="summaryFloatingMasterContainer">
-            <div class='taskOverviewMob'>
-                <div id='taskInBoard'>
-                <div class="containerBox" >
-                    <span id='taskInBoardAmount'>${allYourTasksAmount}</span>
-                    <p>Task in Board</p>
+            <div class="summaryDisplayMobilContainer">
+                <div class='taskOverviewMob'>
+                    <div id='taskInBoard' class="cardsSummaryTop">
+                    <div class="containerBox" >
+                        <span id='taskInBoardAmount'>${allYourTasksAmount}</span>
+                        <p>Task in Board</p>
+                        </div>
+                    </div>
+                    <div id='taskInProgress' class="cardsSummaryTop"><span id='taskInProgressAmount'>${allYourInProgressTasksAmount}</span>
+                        <p>Task in Progress</p>
+                    </div>
+                    <div id='awaitingFeedback' class="cardsSummaryTop"><span
+                            id='awaitingFeedbackAmount'>${allYourAwaitingFeedbackTasksAmount}</span>
+                        <p>Awaiting Feedback</p>
                     </div>
                 </div>
-                <div id='taskInProgress'><span id='taskInProgressAmount'>${allYourInProgressTasksAmount}</span>
-                    <p>Task in Progress</p>
-                </div>
-                <div id='awaitingFeedback'><span
-                        id='awaitingFeedbackAmount'>${allYourAwaitingFeedbackTasksAmount}</span>
-                    <p>Awaiting Feedback</p>
-                </div>
-            </div>
 
-            <div class='ugencySummaryMob' onmouseover="ugencySummaryHoverOn()" onmouseout="ugencySummaryHoverOff()">
-                <div class="ugentMob">
-                    <div class='ugentImgContainerMob'>
-                        <img class='ugentImgMob' id='urgentImg' src='../../assets/img/summary_urgent.png'>
+                <div class='ugencySummaryMob' onmouseover="ugencySummaryHoverOn()" onmouseout="ugencySummaryHoverOff()">
+                    <div class="ugentMob">
+                        <div class='ugentImgContainerMob'>
+                            <img class='ugentImgMob' id='urgentImg' src='../../assets/img/summary_urgent.png'>
+                        </div>
+                        <div class='ugentAmountMob'>
+                            <span id='ugencySummaryAmount'>${yourUrgentTasksAmount}</span>
+                            <p id='ugencySummaryurgent'>Urgent</p>
+                        </div>
+                        <img src='../../assets/img/vertical-line2.png' class='ugentVerticalLine'>
                     </div>
-                    <div class='ugentAmountMob'>
-                        <span id='ugencySummaryAmount'>${yourUrgentTasksAmount}</span>
-                        <p id='ugencySummaryurgent'>Urgent</p>
+                    <div class='deadlineDataMob'>
+                        <p id='deadlineDate'>October 16, 2022</p>
+                        <p class='deadlineText' id='deadlineText'><b>31.01.2023 Deadline</b></p>
                     </div>
-                    <img src='../../assets/img/vertical-line2.png' class='ugentVerticalLine'>
                 </div>
-                <div class='deadlineDataMob'>
-                    <p id='deadlineDate'>October 16, 2022</p>
-                    <p class='deadlineText' id='deadlineText'><b>31.01.2023 Deadline</b></p>
-                </div>
-            </div>
 
-            <div class='toDoDataMob'>
-                <div class='toDo' id='toDo' onmouseover="toDoHoverOn()" onmouseout="toDoHoverOff()">
-                    <img id='toDoImg' src='../../assets/img/to_do_pen.png' alt="">
-                    <div class='toDoAmountData'>
-                        <span id='toDoAmountTasks'>${allYourToDoTasksAmount}</span>
-                        <p id='toDoAmountP'>to-Do</p>
+                <div class='toDoDataMob'>
+                    <div class='toDo' id='toDo' onmouseover="toDoHoverOn()" onmouseout="toDoHoverOff()">
+                        <img id='toDoImg' src='../../assets/img/to_do_pen.png' alt="">
+                        <div class='toDoAmountData'>
+                            <span id='toDoAmountTasks'>${allYourToDoTasksAmount}</span>
+                            <p id='toDoAmountP'>to-Do</p>
+                        </div>
                     </div>
-                </div>
-                <div class='toDoDone' id='toDoDone' onmouseover="toDoDoneHoverOn()" onmouseout="toDoDoneHoverOff()">
-                    <img id='toDoDoneImg' src='../../assets/img/done.png' alt="">
-                    <div class='toDoAmountData'>
-                        <span id='toDoDoneAmountTasks'>${allYourDoneTasksAmount}</span>
-                        <p id='toDoDoneAmountP'>Done</p>
+                    <div class='toDoDone' id='toDoDone' onmouseover="toDoDoneHoverOn()" onmouseout="toDoDoneHoverOff()">
+                        <img id='toDoDoneImg' src='../../assets/img/done.png' alt="">
+                        <div class='toDoAmountData'>
+                            <span id='toDoDoneAmountTasks'>${allYourDoneTasksAmount}</span>
+                            <p id='toDoDoneAmountP'>Done</p>
+                        </div>
                     </div>
                 </div>
             </div>
         </div>`;
 }
 
-
 // LogOut Overlay //
 ////////////////////
 function logOutMasterContainerMob() {
-    document.getElementById('logOutMasterContainer').innerHTML = /*html*/ `
+	document.getElementById('logOutMasterContainer').innerHTML = /*html*/ `
     <div class="logOutNav" onclick="renderHelpMob()">
             <span>Help</span>
         </div>
@@ -133,55 +132,65 @@ function logOutMasterContainerMob() {
 }
 
 function logOutBtnMob() {
-    document.getElementById('logOutMasterContainer').classList.toggle('d-none');
+	document.getElementById('logOutMasterContainer').classList.toggle('d-none');
 }
 
 function logOutMob() {
-    helloCheck = 0;
-    window.location.href = './indexMob.html';
+	helloCheck = 0;
+	window.location.href = './indexMob.html';
 }
 ////////////////////
-
 
 // BackArrow from Help and Legal notice Function //
 ///////////////////////////////////////////////////
 let backButtonMobArray = [
-    { name: '0' },
-    { func: () => { initMobilSummary() } },
-    { func: () => { initMobilBoard() } },
-    { name: '0' },
-    { func: () => { initMobilContacts() } },
+	{ name: '0' },
+	{
+		func: () => {
+			initMobilSummary();
+		},
+	},
+	{
+		func: () => {
+			initMobilBoard();
+		},
+	},
+	{ name: '0' },
+	{
+		func: () => {
+			initMobilContacts();
+		},
+	},
 ];
 
 function backButtonMob() {
-    let index = backButtonMobArray[selectedMenuBtnId];
-    index.func();
+	let index = backButtonMobArray[selectedMenuBtnId];
+	index.func();
 }
 ///////////////////////////////////////////////////
-
 
 // UserGreeting in Mobil //
 ///////////////////////////
 let helloCheckMob = 0;
 function helloPageMob() {
-    if (helloCheckMob == 1) {
-        document.getElementById('greetingMasterContainer').classList.add('d-none');
-    } else {
-        document.getElementById('greetingMasterContainer').innerHTML = ``;
-        document.getElementById('greetingMasterContainer').innerHTML = /*html*/ `
+	if (helloCheckMob == 1) {
+		document.getElementById('greetingMasterContainer').classList.add('d-none');
+	} else {
+		document.getElementById('greetingMasterContainer').innerHTML = ``;
+		document.getElementById('greetingMasterContainer').innerHTML = /*html*/ `
             <div class='greetContainerMob'>
                 <span id='greetUser'></span>
                 <span class="greetUserName">${allUsers[loggedUser[0]].name}</span>
             </div>
         `;
-        helloCheckMob = 1;
-        greetingMasterContainerHide();
-    }
+		helloCheckMob = 1;
+		greetingMasterContainerHide();
+	}
 }
-
 
 function greetingMasterContainerHide() {
-    setTimeout(() => { document.getElementById('greetingMasterContainer').classList.add('d-none') }, 4000);
+	setTimeout(() => {
+		document.getElementById('greetingMasterContainer').classList.add('d-none');
+	}, 4000);
 }
 ///////////////////////////
-
