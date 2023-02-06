c10412e9d8b8f8e74dc1f377d5e0f6c1b2f7407d
HassSebastian <sebastian.hass76@gmail.com>
2023-02-06 18:46:46 +0100

    new contact bug edit

--- js/mobile_contacts.js
+++ js/mobile_contacts.js
@@ -1,22 +1,22 @@
 async function initMobilContacts() {
-  await includeHTML();
-  await loadTask();
-  selectedMenuButton(4);
-  renderContentMobile();
-  userInAlphabetArray();
-  loadContributorsLetterMob();
-  logOutMasterContainerMob();
-  document.getElementById("greetingMasterContainer").classList.add("d-none");
+    await includeHTML();
+    await loadTask();
+    selectedMenuButton(4);
+    renderContentMobile();
+    userInAlphabetArray();
+    loadContributorsLetterMob();
+    logOutMasterContainerMob();
+    document.getElementById("greetingMasterContainer").classList.add("d-none");
 }
 
 async function renderContentMobile() {
-  document.getElementById("mobilContent").innerHTML = "";
-  document.getElementById("mobilContent").innerHTML +=
-    renderContentMobileHTML();
+    document.getElementById("mobilContent").innerHTML = "";
+    document.getElementById("mobilContent").innerHTML +=
+        renderContentMobileHTML();
 }
 
 function renderContentMobileHTML() {
-  return /*html*/ `
+    return /*html*/ `
         <div id="Frame_97Mob" class="Frame_97Mob">
             <div class="Contact_listMob" id="Contact_list"></div>
         </div>
@@ -98,30 +98,32 @@ function renderContentMobileHTML() {
 }
 
 function contactSucc() {
-  document.getElementById("contactSucc").classList.add("contactSuccSlide");
-  setTimeout(closeOverlayMob, 2000);
+    document.getElementById("contactSucc").classList.add("contactSuccSlide");
+    setTimeout(closeOverlayMob, 2000);
 }
 
 function test() {
-  console.log("ok");
+    console.log("ok");
 }
 
 function openOverlayMob() {
-  document
-    .getElementById("overlayMasterContainerMob")
-    .classList.add("overlayMasterContainerMobSlide");
+    document
+        .getElementById("overlayMasterContainerMob")
+        .classList.add("overlayMasterContainerMobSlide");
 }
 
 function closeOverlayMob() {
-  document
-    .getElementById("overlayMasterContainerMob")
-    .classList.remove("overlayMasterContainerMobSlide");
-    document.getElementById("contactSucc").classList.remove("contactSuccSlide");
-    initMobilContacts();
+    document
+        .getElementById("overlayMasterContainerMob")
+        .classList.remove("overlayMasterContainerMobSlide");
+    if (document.querySelector('.contactSuccSlide')) {
+        document.getElementById("contactSucc").classList.remove("contactSuccSlide");
+        setTimeout(initMobilContacts,500);
+    };
 }
 
 function showContactHTMLMob(name, email, phone, letter, color, i) {
-  return /*html*/ `
+    return /*html*/ `
         <div class="arrowContainerMob" onclick="initMobilContacts()">
             <img src="../../assets/img/back_logo_black.png">
         </div>
