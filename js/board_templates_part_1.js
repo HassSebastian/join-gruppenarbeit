5762151d7a0eb8aaf36c97730bfdc82e5b26bf2c
GreenfieldC <101995422+GreenfieldC@users.noreply.github.com>
2023-02-13 23:31:14 +0100

    board.css update

--- js/board_templates_part_1.js
+++ js/board_templates_part_1.js
@@ -4,78 +4,97 @@
  */
 function boardHtml() {
 	return /*html*/ `
-        <div class='boardOverlay'>
-            <div class='boardHeadline'>
-                <span>Board</span>
-            </div>
-            <div class='inputOutContainer'>
-                <div class='inputContainer'>
-                    <div class='inputInContainer'>
-                        <div class='inputFontContainer'>
-                            <input type="text" id="searchField" required placeholder='Find Task' onfocus='startSearch(event)' autocomplete='off'>
-                        </div>
-                        <div class='vector'></div>
-                        <img src='./assets/img/search_logo.png'>
+    <div class="boardContainer">
+    <div class="boardOverlay">
+        <div class="boardHeadline">
+            <span>Board</span>
+        </div>
+        <div class="inputOutContainer">
+            <div class="inputContainer">
+                <div class="inputInContainer">
+                    <div class="inputFontContainer">
+                        <input
+                            type="text"
+                            id="searchField"
+                            required
+                            placeholder="Find Task"
+                            onfocus="startSearch(event)"
+                            autocomplete="off"
+                        />
                     </div>
+                    <div class="vector"></div>
+                    <img src="./assets/img/search_logo.png" />
                 </div>
-                <button class='addTaskButton' onclick='showAddTaskPopupWindow()'>
-                    <span>Add task</span>
-                    <div class='plusOutContainer'>
-                        <img src='./assets/img/plus_logo_white.png'>
-                    </div>
-                </button>
+            </div>
+            <button class="addTaskButton" onclick="showAddTaskPopupWindow()">
+                <span>Add task</span>
+                <div class="plusOutContainer">
+                    <img src="./assets/img/plus_logo_white.png" />
+                </div>
+            </button>
+        </div>
+    </div>
+    <div class="boardSubheaders">
+        <div class="boardSubtitleContainer">
+            <div class="toDoAreaHeader">
+                <span>To do</span>
+                <button class="menuPlusButton" onclick="showAddTaskPopupWindow()"></button>
+            </div>
+            <div class="inProgressAreaHeader">
+                <span>In progress</span>
+                <button class="menuPlusButton" onclick="showAddTaskPopupWindow()"></button>
+            </div>
+            <div class="awaitingFeedbackAreaHeader">
+                <span>Anwaiting Feedback</span>
+                <button class="menuPlusButton" onclick="showAddTaskPopupWindow()"></button>
+            </div>
+
+            <div class="doneAreaHeader">
+                <span>Done</span>
+                <button class="menuPlusButton" onclick="showAddTaskPopupWindow()"></button>
             </div>
         </div>
-        <div class='boardSubheaders'>
-            <div class='boardSubtitleContainer'>
-                <div class='toDoAreaHeader'>
-                    <span>To do</span>
-                    <button class='menuPlusButton' onclick='showAddTaskPopupWindow()'>
-                    </button>
-                </div>
-                <div class='inProgressAreaHeader'>
-                    <span>In progress</span>
-                    <button class='menuPlusButton' onclick='showAddTaskPopupWindow()'>
-                    </button>
-                </div>
-                <div class='awaitingFeedbackAreaHeader'>
-                    <span>Anwaiting Feedback</span>
-                    <button class='menuPlusButton' onclick='showAddTaskPopupWindow()'>
-                    </button>
-                </div>
-                
-                <div class='doneAreaHeader'>
-                    <span>Done</span>
-                        <button class='menuPlusButton' onclick='showAddTaskPopupWindow()'>
-                    </button>
-                </div>
-            </div>
+    </div>
+    <div class="canbanBoard" onscroll="changeHeightDropArea()" id="canbanBoard">
+        <div
+            class="canbanContainer dragArea"
+            id="dropArea0"
+            ondrop="moveTo(0); removeHighlight('dropArea0')"
+            ondragleave="removeHighlight('dropArea0')"
+            ondragover="allowDrop(event); highlight('dropArea0')"
+        >
+            <div id="toDoDiv"></div>
         </div>
-        <div class='canbanBoard' onscroll='changeHeightDropArea()' id='canbanBoard'>
-        <div class="canbanContainer dragArea" id='dropArea0' ondrop="moveTo(0); removeHighlight('dropArea0')"  ondragleave="removeHighlight('dropArea0')" ondragover="allowDrop(event); highlight('dropArea0')">
-                <div id='toDoDiv'>
-                    
-                </div>
-            </div>
-            <div class="canbanContainer dragArea" id='dropArea1' ondrop="moveTo(1); removeHighlight('dropArea1')" ondragleave="removeHighlight('dropArea1')" ondragover="allowDrop(event); highlight('dropArea1')">
-                <div id='progressDiv'>
-                    
-                </div>
-            </div>
-            <div class="canbanContainer dragArea" id='dropArea2' ondrop="moveTo(2); removeHighlight('dropArea2')" ondragleave="removeHighlight('dropArea2')" ondragover="allowDrop(event); highlight('dropArea2')">
-                <div id='awaitingDiv'>
-                    
-                </div>
-            </div>
-            <div class="canbanContainer dragArea" id='dropArea3' ondrop="moveTo(3); removeHighlight('dropArea3')" ondragleave="removeHighlight('dropArea3')" ondragover="allowDrop(event); highlight('dropArea3')">
-                <div id='doneDiv'>
-                    
-                </div>
-            </div>
+        <div
+            class="canbanContainer dragArea"
+            id="dropArea1"
+            ondrop="moveTo(1); removeHighlight('dropArea1')"
+            ondragleave="removeHighlight('dropArea1')"
+            ondragover="allowDrop(event); highlight('dropArea1')"
+        >
+            <div id="progressDiv"></div>
         </div>
-        <div class='shadowOverlay d-none' id='boardPopup' onclick='disablePopupWindow()'>
-           
-        </div>`;
+        <div
+            class="canbanContainer dragArea"
+            id="dropArea2"
+            ondrop="moveTo(2); removeHighlight('dropArea2')"
+            ondragleave="removeHighlight('dropArea2')"
+            ondragover="allowDrop(event); highlight('dropArea2')"
+        >
+            <div id="awaitingDiv"></div>
+        </div>
+        <div
+            class="canbanContainer dragArea"
+            id="dropArea3"
+            ondrop="moveTo(3); removeHighlight('dropArea3')"
+            ondragleave="removeHighlight('dropArea3')"
+            ondragover="allowDrop(event); highlight('dropArea3')"
+        >
+            <div id="doneDiv"></div>
+        </div>
+    </div>
+</div>
+<div class="shadowOverlay d-none" id="boardPopup" onclick="disablePopupWindow()"></div>`;
 }
 
 /**
