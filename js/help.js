4e7dbe854a82d8159efab59ae0a8fc00c2237d68
GreenfieldC <101995422+GreenfieldC@users.noreply.github.com>
2023-02-24 13:02:01 +0100

    help redone responsive done

--- js/help.js
+++ js/help.js
@@ -10,45 +10,64 @@ async function renderHelp() {
 	document.getElementById('content').innerHTML = '';
 	await enableHelp();
 	document.getElementById('content').innerHTML += /*html*/ `
-            <img src="./assets/img/back_logo_black.png" onclick="back()" class="backArrowimg">
-            <span class="helpOverlayspan">Help</span>
-        <div class="textFieldContainer">
-            <div class="whatIsJoinContainer">
-                <span class="whatIsJoinField">What is Join?</span>
-                <span class="whatIsJoinIllustration">
-                    Join is a classical task management tool with a kanban-board. <br>
-                    You can organize your tasks whithin a project or business processes. <br>
-                    It is perfectly suitable for working together with your team. <br>
-                    Join let you and your team work more efficiently. <br>
-                    You have all of your taks in focus und won't miss upcoming deadlines anymore. <br>
+    <div class="helpContainer">
+    <div class="helpContentContainer">
+        <div class="headingHelpContainer">
+            <h1>Help</h1>
+            <img
+                onclick="back()"
+                class="back"
+                src="./assets/img/back_logo_black.png"
+                alt="back to summary"
+            />
+        </div>
+        <div class="helpMainContent">
+            <div class="definitionContainer">
+                <h2>What is Join?</h2>
+                <p class="joinDefinition">
+                    Join is a classical task management tool with a
+                    kanban-board. <br />
+                    You can organize your tasks whithin a project or
+                    business processes. <br />
+                    It is perfectly suitable for working together with
+                    your team. <br />
+                    Join let you and your team work more efficiently.
+                    <br />
+                    You have all of your taks in focus und won't miss
+                    upcoming deadlines anymore. <br />
                     Start working with Join now!
-                </span>
+                </p>
             </div>
-            <span class="howToUseItField">How to use it</span>
-            <div class="listBigContainer">
-                <div class="listContainer">
-                    <span class="listNumber">1.</span>
-                    <span class="listContent">
-                        Add a task with title, description and category. <br>
-                        Assign the task to other team members of your contact list or invite new contacts.
-                    </span>
-                </div>
-                <div class="listContainer">
-                    <span class="listNumber">2.</span>
-                    <span class="listContent">
-                        Check the current status of your tasks in the kanban-board. <br>
-                        You can drag and drop your task to change the status. Also it is possible to edit the task
-                        details.
-                    </span>
-                </div>
-                <div class="listContainer">
-                    <span class="listNumber">3.</span>
-                    <span class="listContent">
-                        Add team members to your contact list to communicate to them fastly. <br>
-                        Also you can assign tasks directly to your contacts.
-                    </span>
+            <div class="manualContainer">
+                <h2 class="subheading">How to use it</h2>
+                <div class="stepsManualContainer">
+                    <div class="steps">
+                        <p>
+                            Add a task with title, description and
+                            category.  Assign the task to other team
+                            members of your contact list or invite new
+                            contacts.
+                        </p>
+                    </div>
+                    <div class="steps">
+                        <p>
+                            Check the current status of your tasks in
+                            the kanban-board. You can drag and drop your
+                            task to change the status. Also it is
+                            possible to edit the task details.
+                        </p>
+                    </div>
+                    <div class="steps">
+                        <p>
+                            Add team members to your contact list to
+                            communicate to them fastly. Also you can
+                            assign tasks directly to your contacts.
+                        </p>
+                    </div>
                 </div>
             </div>
         </div>
+    </div>
+</div>
     `;
 }
