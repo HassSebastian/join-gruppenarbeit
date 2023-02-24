1e8388b23f20e7e7f0cc2d179fc94c9e4f14c7da
GreenfieldC <101995422+GreenfieldC@users.noreply.github.com>
2023-02-24 09:21:25 +0100

    addTask line 68 fakeUsers commented out due bugs

--- js/add_task.js
+++ js/add_task.js
@@ -66,13 +66,16 @@ function transferallUserData() {
 
 /**
  * Creates a copy of allUsers without password
+ * !Ich habe hier auskommentiert, weil es nur Fehler rausgibt.
+ * Um keinen Fehler in der Anzeige zu verursachen, muss der guest in
+ * allUsers und allFakeUser an der selben Stelle im jeweiligen Array stehen!
  */
 function creatingTransferObjectOfContacts() {
-	const users = guestLoggedIn
+	/* const users = guestLoggedIn
 		? allFakeUsers
-		: allUsers; /* .filter((user) => user.email !== guestEmail) */
+		: allUsers; */
 
-	users.forEach((user) => {
+	allUsers.forEach((user) => {
 		transferArray.push({
 			colorIndex: user.colorIndex,
 			email: user.email,
@@ -146,18 +149,12 @@ function joinTaskArrayExistInStorage() {
  */
 function enableDisableCatList() {
 	if (categoryListAndNewCategoryInputNotActive()) {
-		document
-			.getElementById('CatListDropdown')
-			.classList.remove('listD-none');
-		document
-			.getElementById('addTaskAssignedBox')
-			.classList.add('addMarginTop');
+		document.getElementById('CatListDropdown').classList.remove('listD-none');
+		document.getElementById('addTaskAssignedBox').classList.add('addMarginTop');
 		borderBottomOffAssignedBoxButton('selectedCat');
 	} else {
 		document.getElementById('CatListDropdown').classList.add('listD-none');
-		document
-			.getElementById('addTaskAssignedBox')
-			.classList.remove('addMarginTop');
+		document.getElementById('addTaskAssignedBox').classList.remove('addMarginTop');
 		borderBottomOnAssignedBoxButton('selectedCat');
 	}
 	catListStatus = !catListStatus;
@@ -190,11 +187,9 @@ function renderCategoryList() {
 		let categoryName = addTaskCategoryList[i]['category'];
 		let categoryColor = addTaskCategoryList[i]['catColor'];
 		if (categoryColorAvailable(categoryColor)) {
-			document.getElementById('CatListDropdown').innerHTML +=
-				dropdownCategoryListHtml(categoryName, categoryColor, i);
+			document.getElementById('CatListDropdown').innerHTML += dropdownCategoryListHtml(categoryName, categoryColor, i);
 		} else {
-			document.getElementById('CatListDropdown').innerHTML +=
-				dropdownCategoryListHtml1(categoryName, i);
+			document.getElementById('CatListDropdown').innerHTML += dropdownCategoryListHtml1(categoryName, i);
 		}
 	}
 }
@@ -330,10 +325,7 @@ function setSettingsForNewCategoryInput() {
 function setSettingsForExistingCategory(catId) {
 	let newCat = addTaskCategoryList[catId]['category'];
 	let categoryColor = addTaskCategoryList[catId]['catColor'];
-	document.getElementById('selectedCat').innerHTML = existingCategoryHtml(
-		newCat,
-		categoryColor
-	);
+	document.getElementById('selectedCat').innerHTML = existingCategoryHtml(newCat, categoryColor);
 	catColor = categoryColor;
 	enableDisableCatList();
 	document.getElementById('dropdownImg').classList.remove('d-none');
@@ -346,14 +338,10 @@ function setSettingsForExistingCategory(catId) {
  */
 function addColorToCat(colorId) {
 	if (catColor != '' || catColor == '0') {
-		document
-			.getElementById('color' + catColor + 'Div')
-			.classList.remove('colorDivSelected');
+		document.getElementById('color' + catColor + 'Div').classList.remove('colorDivSelected');
 		catColor = '';
 	}
-	document
-		.getElementById('color' + colorId + 'Div')
-		.classList.add('colorDivSelected');
+	document.getElementById('color' + colorId + 'Div').classList.add('colorDivSelected');
 	catColor = colorId;
 }
 
@@ -361,18 +349,14 @@ function addColorToCat(colorId) {
  * this function show a popup, that indicated that the new task is succsessfully created.
  */
 function showAddDiv() {
-	document
-		.getElementById('taskCreatedIndication')
-		.classList.add('taskCreatedIndication');
+	document.getElementById('taskCreatedIndication').classList.add('taskCreatedIndication');
 }
 
 /**
  * this function inhibited to show a popup, that indicated that the new task is succsessfuly created.
  */
 function notShowAddDiv() {
-	document
-		.getElementById('taskCreatedIndication')
-		.classList.remove('taskCreatedIndication');
+	document.getElementById('taskCreatedIndication').classList.remove('taskCreatedIndication');
 }
 
 /**
@@ -621,8 +605,7 @@ async function deleteJoinTaskArrayFromServer() {
 
 /******************************************************************************** */
 function addTaskClearOn() {
-	document.getElementById('addTaskClear').src =
-		'././assets/img/close_logo_blue.png';
+	document.getElementById('addTaskClear').src = '././assets/img/close_logo_blue.png';
 }
 
 function addTaskClearOff() {
@@ -648,29 +631,17 @@ function btnNotSelected(cListLength) {
 }
 
 function selectPrioBtn(selectedId, btnName) {
-	document
-		.getElementById(selectedId)
-		.classList.add(`${btnName.toLowerCase()}-color`);
+	document.getElementById(selectedId).classList.add(`${btnName.toLowerCase()}-color`);
 
-	document
-		.getElementById(`addTask${btnName}Span`)
-		.classList.add('color-white');
-	document.getElementById(
-		`addTask${btnName}Img`
-	).src = `./assets/img/${btnName.toLowerCase()}_white.png`;
+	document.getElementById(`addTask${btnName}Span`).classList.add('color-white');
+	document.getElementById(`addTask${btnName}Img`).src = `./assets/img/${btnName.toLowerCase()}_white.png`;
 	prio = btnName;
 }
 
 function removeBtnSelection(btnName) {
-	document
-		.getElementById(`addTask${btnName}`)
-		.classList.remove(`${btnName.toLowerCase()}-color`);
-	document
-		.getElementById(`addTask${btnName}Span`)
-		.classList.remove('color-white');
-	document.getElementById(
-		`addTask${btnName}Img`
-	).src = `./assets/img/${btnName.toLowerCase()}.png`;
+	document.getElementById(`addTask${btnName}`).classList.remove(`${btnName.toLowerCase()}-color`);
+	document.getElementById(`addTask${btnName}Span`).classList.remove('color-white');
+	document.getElementById(`addTask${btnName}Img`).src = `./assets/img/${btnName.toLowerCase()}.png`;
 }
 
 function unselectOtherBtn(idList) {
@@ -679,15 +650,9 @@ function unselectOtherBtn(idList) {
 		let cListLength = document.getElementById(selectedId).classList.length;
 		let btnName = selectedId.replace('addTask', '');
 		if (btnIsSelected(cListLength)) {
-			document
-				.getElementById(`addTask${btnName}`)
-				.classList.remove(`${btnName.toLowerCase()}-color`);
-			document
-				.getElementById(`addTask${btnName}Span`)
-				.classList.remove('color-white');
-			document.getElementById(
-				`addTask${btnName}Img`
-			).src = `./assets/img/${btnName.toLowerCase()}.png`;
+			document.getElementById(`addTask${btnName}`).classList.remove(`${btnName.toLowerCase()}-color`);
+			document.getElementById(`addTask${btnName}Span`).classList.remove('color-white');
+			document.getElementById(`addTask${btnName}Img`).src = `./assets/img/${btnName.toLowerCase()}.png`;
 		}
 	}
 }
@@ -772,9 +737,7 @@ async function subtaskListHtml() {
 }
 
 function subtaskSelectionChange(subTaskIndex) {
-	let actualSubTaskStatus = document.getElementById(
-		`subtask${subTaskIndex}`
-	).checked;
+	let actualSubTaskStatus = document.getElementById(`subtask${subTaskIndex}`).checked;
 	if (actualSubTaskStatus) {
 		subTaskArray[subTaskIndex]['subtaskStatus'] = true;
 	} else {
@@ -862,9 +825,7 @@ function borderBottomOnAssignedBoxButton(id) {
  * Sets the placeholder of inputField to "contact email"
  */
 function assignChangeInputPlaceholderToContactEmail() {
-	document.getElementsByName(
-		'selectedAssign'
-	)[0].placeholder = `Contact email`;
+	document.getElementsByName('selectedAssign')[0].placeholder = `Contact email`;
 }
 
 /**
@@ -878,9 +839,7 @@ function enableInputaddTasAssign() {
  * Shows btn to delete or confirm filled in input
  */
 function showCancelConfirmButtons() {
-	document
-		.getElementById('assignToCancelConfirmImgContainer')
-		.classList.remove('d-none');
+	document.getElementById('assignToCancelConfirmImgContainer').classList.remove('d-none');
 }
 
 /**
@@ -914,9 +873,7 @@ function doNotCloseOnClick(event) {
 }
 
 function assignInputPlaceholderToDefaultMode() {
-	document.getElementsByName(
-		'selectedAssign'
-	)[0].placeholder = `Select contacts to Assign`;
+	document.getElementsByName('selectedAssign')[0].placeholder = `Select contacts to Assign`;
 }
 
 function assignInputValueToDefault() {
@@ -924,9 +881,7 @@ function assignInputValueToDefault() {
 }
 
 function hideCancelConfirmButtons() {
-	document
-		.getElementById('assignToCancelConfirmImgContainer')
-		.classList.add('d-none');
+	document.getElementById('assignToCancelConfirmImgContainer').classList.add('d-none');
 }
 
 function showAssignDropDownImg() {
@@ -942,9 +897,7 @@ function changeAssignPlaceholderColorToGrey() {
 }
 
 function changeAssignPlaceholderColorToDefault() {
-	document
-		.getElementById('selectedAssign')
-		.classList.remove('greyPlaceholder');
+	document.getElementById('selectedAssign').classList.remove('greyPlaceholder');
 }
 
 function assignBoxBackToDefaultMode() {
@@ -1005,11 +958,7 @@ function removeSelectedContactFromTaskForce(index) {
  * @param contact - the contact that was selected
  * @param indexOfMemberInTaskForce - The index of the contact in the task force array.
  */
-function addRemoveToggleForTaskForce(
-	addedToTaskForce,
-	contact,
-	indexOfMemberInTaskForce
-) {
+function addRemoveToggleForTaskForce(addedToTaskForce, contact, indexOfMemberInTaskForce) {
 	if (!addedToTaskForce) {
 		addCheckMarkToCheckBox(contact);
 		addSelectedContactToTaskForce(contact);
@@ -1031,11 +980,7 @@ function addContactToTaskForceWithCheckBox(contact) {
 	let addedToTaskForce = coworkersToAssignTo[contact].check;
 	let emailAddress = coworkersToAssignTo[contact].email;
 	let indexOfMemberOfTaskForce = findIndexOfMemberOfTaskForce(emailAddress);
-	addRemoveToggleForTaskForce(
-		addedToTaskForce,
-		contact,
-		indexOfMemberOfTaskForce
-	);
+	addRemoveToggleForTaskForce(addedToTaskForce, contact, indexOfMemberOfTaskForce);
 	addedToTaskForce = !addedToTaskForce;
 	coworkersToAssignTo[contact].check = addedToTaskForce;
 }
@@ -1048,11 +993,7 @@ async function renderContactsInAssignDropDownMenu() {
 	let assignedContactList = document.getElementById('dropdown2');
 	coworkersToAssignTo.forEach((coworker, contact) => {
 		if (notLoggedUser(contact)) {
-			assignedContactList.innerHTML +=
-				generateAssignContactListForDropDownMenu(
-					coworker.name,
-					contact
-				);
+			assignedContactList.innerHTML += generateAssignContactListForDropDownMenu(coworker.name, contact);
 		}
 	});
 }
@@ -1115,12 +1056,7 @@ function renderBadgesMemberOfTaskForce() {
 		const badgesIndex = member.colorIndex;
 
 		/* chooseColorForTaskForceBadge(initialFirstName, initialLastName); */
-		badgeContainer.innerHTML += generateBadgesTaskForceHtml(
-			i,
-			name,
-			initials,
-			badgesIndex
-		);
+		badgeContainer.innerHTML += generateBadgesTaskForceHtml(i, name, initials, badgesIndex);
 	});
 }
 
@@ -1161,9 +1097,7 @@ function clearTaskForce() {
  * Opens window with a message
  */
 function frontEndDeveloper() {
-	alert(
-		'This function is part of backend. The course is about frontend though'
-	);
+	alert('This function is part of backend. The course is about frontend though');
 }
 
 /* 
