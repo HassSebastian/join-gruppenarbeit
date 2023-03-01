0697b36aea6d1e5660cc25f375711fa58e85c44d
GreenfieldC <101995422+GreenfieldC@users.noreply.github.com>
2023-03-01 11:57:31 +0100

    generic function added

--- js/generic_functions.js
+++ js/generic_functions.js
@@ -2,3 +2,11 @@
 function setInnerHtmlById(id, html) {
 	document.getElementById(id).innerHTML = html;
 }
+
+function showElementById(id) {
+	document.getElementById(id).classList.remove('d-none');
+}
+
+function hideElementById(id) {
+	document.getElementById(id).classList.add('d-none');
+}
