7be0fd578e12cd9d65221a5d965ba6d24e638831
GreenfieldC <101995422+GreenfieldC@users.noreply.github.com>
2023-03-03 17:19:01 +0100

    responsive shortened

--- js/responsiv.js
+++ js/responsiv.js
@@ -3,16 +3,7 @@ let sliderMenuShown = false;
 // responsiv Slider menu functions
 function enableDisableSliderMenu() {
 	let sliderMenu = document.querySelector('.sliderMenu');
-
 	sliderMenu.classList.toggle('showSliderMenu');
-
-	/* 	if (!sliderMenuShown) {
-		sliderMenu.classList.add('showSliderMenu');
-		sliderMenuShown = true;
-	} else {
-		sliderMenu.classList.remove('showSliderMenu');
-		sliderMenuShown = false;
-	} */
 }
 
 /**
