c74dd33476b498175ba2a651cb870f262a33a494
GreenfieldC <101995422+GreenfieldC@users.noreply.github.com>
2023-03-03 17:14:30 +0100

    content overflow hidden

--- js/responsiv.js
+++ js/responsiv.js
@@ -3,13 +3,16 @@ let sliderMenuShown = false;
 // responsiv Slider menu functions
 function enableDisableSliderMenu() {
 	let sliderMenu = document.querySelector('.sliderMenu');
-	if (!sliderMenuShown) {
+
+	sliderMenu.classList.toggle('showSliderMenu');
+
+	/* 	if (!sliderMenuShown) {
 		sliderMenu.classList.add('showSliderMenu');
 		sliderMenuShown = true;
 	} else {
 		sliderMenu.classList.remove('showSliderMenu');
 		sliderMenuShown = false;
-	}
+	} */
 }
 
 /**
